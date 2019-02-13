193a3960315b553d0d40f74cdfbff0bdbcce69b2
Lu Lu <erlululu@gmail.com>
2019-02-12 22:49:16 -0500

    add example: dataset

---
+++ examples/dataset.py
@@ -0,0 +1,39 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import time
+
+import numpy as np
+
+import sciconet as scn
+
+
+def main():
+    fname_train = 'examples/dataset.train'
+    fname_test = 'examples/dataset.test'
+    data = scn.data.DataSet(fname_train=fname_train, fname_test=fname_test, col_x=(0,), col_y=(1,))
+
+    x_dim, y_dim = 1, 1
+    layer_size = [x_dim] + [50] * 3 + [y_dim]
+    activation = 'tanh'
+    initializer = 'Glorot normal'
+    net = scn.FNN(layer_size, activation, initializer)
+
+    model = scn.Model(data, net)
+
+    optimizer = 'adam'
+    lr = 0.001
+    batch_size = 0
+    ntest = 0
+    model.compile(optimizer, lr, batch_size, ntest, metrics=['l2 relative error'])
+
+    epochs = 50000
+    losshistory, training_state = model.train(epochs)
+
+    scn.saveplot(losshistory, np.hstack((training_state.X_test, training_state.y_test, training_state.best_y)),
+                 x_dim, y_dim, issave=True, isplot=True)
+
+
+if __name__ == '__main__':
+    main()
--- sciconet/data/__init__.py
+++ sciconet/data/__init__.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
 
+from .dataset import DataSet
 from .func import Func
 from .pde import PDE
