24c575fcb3e9670802530d822c938e3a05df6644
lululxvi <erlululu@gmail.com>
2019-02-16 17:44:31 -0500

    refactor FNN

--- sciconet/maps/fnn.py
+++ sciconet/maps/fnn.py
@@ -3,9 +3,7 @@ from __future__ import division
 from __future__ import print_function
 
 import math
-import sys
 
-import numpy as np
 import tensorflow as tf
 
 from . import activations
@@ -40,70 +38,52 @@ class FNN(object):
         self.dropout = tf.placeholder(tf.bool)
         self.data_id = tf.placeholder(tf.uint8)
         self.x = tf.placeholder(config.real(tf), [None, self.layer_size[0]])
+
         y = self.x
         for i in range(len(self.layer_size) - 2):
-            y = self.add_layer(y, self.layer_size[i + 1], False, self.training)
+            if self.batch_normalization is None:
+                y = self.dense(y, self.layer_size[i + 1], activation=self.activation)
+            elif self.batch_normalization == 'before':
+                y = self.dense_batchnorm_v1(y, self.layer_size[i + 1])                
+            elif self.batch_normalization == 'after':
+                y = self.dense_batchnorm_v2(y, self.layer_size[i + 1])
+            else:
+                raise ValueError('batch_normalization')
             if self.dropout_rate > 0:
                 y = tf.layers.dropout(y, rate=self.dropout_rate, training=self.dropout)
-        self.y = self.add_layer(y, self.layer_size[-1], True, self.training)
+        self.y = self.dense(y, self.layer_size[-1])
 
         self.y_ = tf.placeholder(config.real(tf), [None, self.layer_size[-1]])
 
-    def dense(self, inputs, units, activation=None, use_bias=True,
-              kernel_initializer=None, bias_initializer=tf.zeros_initializer(),
-              kernel_regularizer=None, bias_regularizer=None,
-              activity_regularizer=None, kernel_constraint=None,
-              bias_constraint=None, trainable=True, name=None, reuse=None):
-        if False:
-            shape = inputs.get_shape().as_list()
-            fan_in = shape[1]
-            W = tf.Variable(tf.random_normal([fan_in, units],
-                                             stddev=math.sqrt(2 / fan_in)))
-            # weight normalization
-            g = tf.Variable(tf.ones(units))
-            W = tf.nn.l2_normalize(W, axis=0) * g
-            y = tf.matmul(inputs, W)
-            if use_bias:
-                b = tf.Variable(tf.zeros(units))
-                y += b
-            if activation is not None:
-                return activation(y)
-            return y
+    def dense(self, inputs, units, activation=None, use_bias=True):
         return tf.layers.dense(
             inputs, units, activation=activation, use_bias=use_bias,
-            kernel_initializer=kernel_initializer,
-            bias_initializer=bias_initializer,
-            kernel_regularizer=kernel_regularizer,
-            bias_regularizer=bias_regularizer,
-            activity_regularizer=activity_regularizer,
-            kernel_constraint=kernel_constraint,
-            bias_constraint=bias_constraint, trainable=trainable, name=name,
-            reuse=reuse)
+            kernel_initializer=self.kernel_initializer,
+            kernel_regularizer=self.regularizer,
+            bias_regularizer=self.regularizer)
 
-    def add_layer(self, inputs, units, last_layer, training):
-        if last_layer:
-            return self.dense(
-                inputs, units, kernel_initializer=self.kernel_initializer,
-                kernel_regularizer=self.regularizer, bias_regularizer=self.regularizer)
-        if self.batch_normalization is None:
-            return self.dense(
-                inputs, units, activation=self.activation,
-                kernel_initializer=self.kernel_initializer,
-                kernel_regularizer=self.regularizer, bias_regularizer=self.regularizer)
-        if self.batch_normalization == 'before':
-            # FC - BN - activation
-            y = self.dense(
-                inputs, units, use_bias=False,
-                kernel_initializer=self.kernel_initializer,
-                kernel_regularizer=self.regularizer)
-            y = tf.layers.batch_normalization(y, training=training)
-            return self.activation(y)
-        elif self.batch_normalization == 'after':
-            # FC - activation - BN
-            y = self.dense(
-                inputs, units, activation=self.activation,
-                kernel_initializer=self.kernel_initializer,
-                kernel_regularizer=self.regularizer, bias_regularizer=self.regularizer)
-            return tf.layers.batch_normalization(y, training=training)
-        else:
-            raise ValueError('batch_normalization')
+    def dense_weightnorm(self, inputs, units, activation=None, use_bias=True):
+        shape = inputs.get_shape().as_list()
+        fan_in = shape[1]
+        W = tf.Variable(tf.random_normal([fan_in, units],
+                                         stddev=math.sqrt(2 / fan_in)))
+        g = tf.Variable(tf.ones(units))
+        W = tf.nn.l2_normalize(W, axis=0) * g
+        y = tf.matmul(inputs, W)
+        if use_bias:
+            b = tf.Variable(tf.zeros(units))
+            y += b
+        if activation is not None:
+            return activation(y)
+        return y
+    
+    def dense_batchnorm_v1(self, inputs, units):
+        # FC - BN - activation
+        y = self.dense(inputs, units, use_bias=False)
+        y = tf.layers.batch_normalization(y, training=self.training)
+        return self.activation(y)
+
+    def dense_batchnorm_v2(self, inputs, units):
+        # FC - activation - BN
+        y = self.dense(inputs, units, activation=self.activation)
+        return tf.layers.batch_normalization(y, training=self.training)
