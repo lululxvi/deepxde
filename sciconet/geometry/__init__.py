c2118a56c797b3049b2a6df094e105a62b0c1e98
Lu Lu <erlululu@gmail.com>
2019-05-08 11:42:23 -0400

    Support Triangle

--- sciconet/geometry/__init__.py
+++ sciconet/geometry/__init__.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 from .geometry_1d import Interval
 from .geometry_2d import Disk
 from .geometry_2d import Rectangle
+from .geometry_2d import Triangle
 from .geometry_nd import Hypercube
 from .geometry_nd import Hypersphere
 from .timedomain import GeometryXTime
--- sciconet/geometry/geometry_2d.py
+++ sciconet/geometry/geometry_2d.py
@@ -151,3 +151,104 @@ class Rectangle(Hypercube):
             else:
                 x.append([self.xmin[0], self.xmax[1] - l + l3])
         return np.vstack((x_corner, x))
+
+
+class Triangle(Geometry):
+    def __init__(self, x1, x2, x3):
+        self.x1 = np.array(x1)
+        self.x2 = np.array(x2)
+        self.x3 = np.array(x3)
+        self.v12 = self.x2 - self.x1
+        self.v23 = self.x3 - self.x2
+        self.v31 = self.x1 - self.x3
+        self.l12 = np.linalg.norm(self.v12)
+        self.l23 = np.linalg.norm(self.v23)
+        self.l31 = np.linalg.norm(self.v31)
+        self.n12 = self.v12 / self.l12
+        self.n23 = self.v23 / self.l23
+        self.n31 = self.v31 / self.l31
+        self.perimeter = self.l12 + self.l23 + self.l31
+        super(Triangle, self).__init__(
+            2,
+            self.l12
+            * self.l23
+            * self.l31
+            / (
+                self.perimeter
+                * (self.l12 + self.l23 - self.l31)
+                * (self.l23 + self.l31 - self.l12)
+                * (self.l31 + self.l12 - self.l23)
+            )
+            ** 0.5,
+        )
+
+    def random_points(self, n, random="pseudo"):
+        """There are two methods for triangle point picking.
+        Method 1 (used here):
+            https://math.stackexchange.com/questions/18686/uniform-random-point-in-triangle
+        Method 2:
+            http://mathworld.wolfram.com/TrianglePointPicking.html
+            https://hbfs.wordpress.com/2010/10/05/random-points-in-a-triangle-generating-random-sequences-ii/
+            https://stackoverflow.com/questions/19654251/random-point-inside-triangle-inside-java
+        """
+        sqrt_r1 = np.sqrt(np.random.rand(n, 1))
+        r2 = np.random.rand(n, 1)
+        return (
+            (1 - sqrt_r1) * self.x1
+            + sqrt_r1 * (1 - r2) * self.x2
+            + r2 * sqrt_r1 * self.x3
+        )
+
+    def uniform_boundary_points(self, n):
+        density = n / self.perimeter
+        x12 = (
+            np.linspace(0, 1, num=int(np.ceil(density * self.l12)), endpoint=False)[
+                :, None
+            ]
+            * self.v12
+            + self.x1
+        )
+        x23 = (
+            np.linspace(0, 1, num=int(np.ceil(density * self.l23)), endpoint=False)[
+                :, None
+            ]
+            * self.v23
+            + self.x2
+        )
+        x31 = (
+            np.linspace(0, 1, num=int(np.ceil(density * self.l31)), endpoint=False)[
+                :, None
+            ]
+            * self.v31
+            + self.x3
+        )
+        x = np.vstack((x12, x23, x31))
+        if n != len(x):
+            print(
+                "Warning: {} points required, but {} points sampled.".format(n, len(x))
+            )
+        return x
+
+    def random_boundary_points(self, n, random="pseudo"):
+        x_corner = np.vstack((self.x1, self.x2, self.x3))
+        n -= 3
+        if n <= 0:
+            return x_corner
+
+        if random == "sobol":
+            u = np.ravel(sobol_sequence.sample(n + 3, 1))[1:]
+            u = u[np.logical_not(np.isclose(u, self.l12 / self.perimeter))]
+            u = u[np.logical_not(np.isclose(u, (self.l12 + self.l23) / self.perimeter))]
+            u = u[:n, None]
+        else:
+            u = np.random.rand(n, 1)
+        u *= self.perimeter
+        x = []
+        for l in u:
+            if l < self.l12:
+                x.append(l * self.n12 + self.x1)
+            elif l < self.l12 + self.l23:
+                x.append((l - self.l12) * self.n23 + self.x2)
+            else:
+                x.append((l - self.l12 - self.l23) * self.n31 + self.x3)
+        return np.vstack((x_corner, x))
