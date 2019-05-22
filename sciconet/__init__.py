525a43f3d732cdb689ff9795fd652b162dfefe75
Lu Lu <erlululu@gmail.com>
2019-05-22 15:49:50 -0400

    Add callbacks: OperatorPredictor, FirstDerivative

--- sciconet/__init__.py
+++ sciconet/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
+from . import callbacks
 from . import data
 from . import geometry
 from . import maps
--- sciconet/callbacks.py
+++ sciconet/callbacks.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import tensorflow as tf
+
 
 class Callback(object):
     """Callback base class.
@@ -15,7 +17,12 @@ class Callback(object):
         self.model = None
 
     def set_model(self, model):
-        self.model = model
+        if model is not self.model:
+            self.model = model
+            self.init()
+
+    def init(self):
+        """Init after setting a model."""
 
     def on_epoch_begin(self):
         """Called at the beginning of every epoch."""
@@ -95,3 +102,47 @@ class CallbackList(Callback):
         if not isinstance(callback, Callback):
             raise Exception(str(callback) + " is an invalid Callback object")
         self.callbacks.append(callback)
+
+
+class OperatorPredictor(Callback):
+    """Generates operator values for the input samples.
+    
+    Args:
+        x: The input data.
+        op: The operator with inputs (x, y).
+    """
+
+    def __init__(self, x, op):
+        super(OperatorPredictor, self).__init__()
+        self.x = x
+        self.op = op
+        self.tf_op = None
+        self.value = None
+
+    def init(self):
+        self.tf_op = self.op(self.model.net.inputs, self.model.net.outputs)
+
+    def on_predict_end(self):
+        self.value = self.model.sess.run(
+            self.tf_op,
+            feed_dict=self.model._get_feed_dict(False, False, 2, self.x, None),
+        )
+
+    def get_value(self):
+        return self.value
+
+
+class FirstDerivative(OperatorPredictor):
+    """Generates the first order derivative of the outputs with respect to the inputs.
+    
+    Args:
+        x: The input data.
+    """
+
+    def __init__(self, x, component_x=0, component_y=0):
+        def first_derivative(x, y):
+            return tf.gradients(y[:, component_y : component_y + 1], x)[0][
+                :, component_x : component_x + 1
+            ]
+
+        super(FirstDerivative, self).__init__(x, first_derivative)
