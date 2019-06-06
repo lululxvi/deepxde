bccc6a7943909b8c2e979f1c0dc34f017fcd7aba
Lu Lu <erlululu@gmail.com>
2019-06-05 21:30:43 -0400

    Refactor IDE

--- examples/ide.py
+++ examples/ide.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 
@@ -17,6 +18,9 @@ def main():
         rhs = 2 * np.pi * tf.cos(2 * np.pi * x) + tf.sin(np.pi * x) ** 2 / np.pi
         return lhs1 + (lhs2 - rhs)[: tf.size(lhs1)]
 
+    def boundary(x, on_boundary):
+        return on_boundary and np.isclose(x[0], 0)
+
     def func(x):
         """
         x: array_like, N x D_in
@@ -25,29 +29,32 @@ def main():
         return np.sin(2 * np.pi * x)
 
     geom = scn.geometry.Interval(0, 1)
+    bc = scn.DirichletBC(geom, func, boundary)
 
-    num_train = 16
-    nbc = 1
-    num_test = 128
     quad_deg = 16
-    data = scn.data.IDE(geom, ide, func, num_train, nbc, num_test, quad_deg)
+    data = scn.data.IDE(
+        geom, ide, bc, quad_deg, num_domain=16, num_boundary=2, func=func
+    )
 
-    x_dim, y_dim = 1, 1
-    layer_size = [x_dim] + [20] * 3 + [y_dim]
+    layer_size = [1] + [20] * 3 + [1]
     activation = "tanh"
     initializer = "Glorot uniform"
     net = scn.maps.FNN(layer_size, activation, initializer)
 
     model = scn.Model(data, net)
+    model.compile("adam", lr=0.001, metrics=["l2 relative error"])
+    losshistory, train_state = model.train(epochs=10000)
 
-    optimizer = "adam"
-    lr = 0.001
-    model.compile(optimizer, lr, metrics=["l2 relative error"])
+    X = geom.uniform_points(100, True)
+    y_true = func(X)
+    y_pred = model.predict(X)
+    print("L2 relative error:", scn.metrics.l2_relative_error(y_true, y_pred))
 
-    epochs = 10000
-    losshistory, train_state = model.train(epochs)
-
-    scn.saveplot(losshistory, train_state, issave=True, isplot=True)
+    plt.figure()
+    plt.plot(X, y_true, "-")
+    plt.plot(X, y_pred, "o")
+    plt.show()
+    np.savetxt("test.dat", np.hstack((X, y_true, y_pred)))
 
 
 if __name__ == "__main__":
--- sciconet/data/ide.py
+++ sciconet/data/ide.py
@@ -5,76 +5,122 @@ from __future__ import print_function
 import numpy as np
 import tensorflow as tf
 
-from .data import Data
+from .pde import PDE
 from .. import config
-from .. import losses
+from .. import losses as losses_module
 from ..utils import run_if_any_none
 
 
-class IDE(Data):
+class IDE(PDE):
     """IDE solver.
-    The current version only supports the 1D problem with initial condition at x = 0.
+    The current version only supports 1D problems with initial condition at x = 0.
     """
 
-    def __init__(self, geom, ide, func, num_train, nbc, num_test, quad_deg):
-        self.geom = geom
-        self.ide = ide
-        self.func = func
-        self.num_train = num_train
-        self.nbc = nbc
-        self.num_test = num_test
+    def __init__(
+        self,
+        geom,
+        ide,
+        bcs,
+        quad_deg,
+        num_domain=0,
+        num_boundary=0,
+        train_distribution="random",
+        anchors=None,
+        func=None,
+        num_test=None,
+    ):
         self.quad_deg = quad_deg
+        self.quad_x, self.quad_w = np.polynomial.legendre.leggauss(quad_deg)
 
         self.train_x, self.train_y = None, None
         self.test_x, self.test_y = None, None
 
-        self.quad_x, self.quad_w = np.polynomial.legendre.leggauss(quad_deg)
+        super(IDE, self).__init__(
+            geom,
+            1,
+            ide,
+            bcs,
+            num_domain=num_domain,
+            num_boundary=num_boundary,
+            train_distribution=train_distribution,
+            anchors=anchors,
+            func=func,
+            num_test=num_test,
+        )
 
-    def losses(self, y_true, y_pred, loss, model):
-        int_mat_train = self.get_int_matrix(self.num_train, True)
-        int_mat_test = self.get_int_matrix(self.num_test, False)
+    def losses(self, y_true, y_pred, loss_type, model):
+        bcs_start = np.cumsum([0] + self.num_bcs)
+        loss_f = losses_module.get(loss_type)
+
+        int_mat_train = self.get_int_matrix(True)
+        int_mat_test = self.get_int_matrix(False)
         f = tf.cond(
             tf.equal(model.net.data_id, 0),
-            lambda: self.ide(model.net.x, y_pred, int_mat_train),
-            lambda: self.ide(model.net.x, y_pred, int_mat_test),
+            lambda: self.pde(model.net.x, y_pred, int_mat_train),
+            lambda: self.pde(model.net.x, y_pred, int_mat_test),
         )
-        return [
-            losses.get(loss)(y_true[: self.nbc], y_pred[: self.nbc]),
-            losses.get(loss)(tf.zeros(tf.shape(f)), f),
-        ]
+        if not isinstance(f, list):
+            f = [f]
+        f = [fi[bcs_start[-1] :] for fi in f]
+        loss = [loss_f(tf.zeros(tf.shape(fi)), fi) for fi in f]
+
+        for i, bc in enumerate(self.bcs):
+            beg, end = bcs_start[i], bcs_start[i + 1]
+            error = (
+                tf.cond(
+                    tf.equal(model.net.data_id, 0),
+                    lambda: bc.error(self.train_x, model.net.x, y_pred, beg, end),
+                    lambda: bc.error(self.test_x, model.net.x, y_pred, beg, end),
+                ),
+            )
+            loss.append(loss_f(tf.zeros(tf.shape(error)), error))
+        return loss
 
     @run_if_any_none("train_x", "train_y")
     def train_next_batch(self, batch_size=None):
-        self.train_x, self.train_y = self.gen_data(self.num_train)
+        self.train_x = self.train_points()
+        x_bc = self.bc_points()
+        x_quad = self.quad_points(self.train_x)
+        self.train_x = np.vstack((x_bc, self.train_x, x_quad))
+        self.train_y = self.func(self.train_x)
         return self.train_x, self.train_y
 
     @run_if_any_none("test_x", "test_y")
     def test(self):
-        self.test_x, self.test_y = self.gen_data(self.num_test)
+        if self.num_test is None:
+            self.test_x = self.train_x
+            self.test_y = self.train_y
+        else:
+            self.test_x = self.test_points()
+            x_quad = self.quad_points(self.test_x)
+            self.test_x = np.vstack(
+                (self.train_x[: sum(self.num_bcs)], self.test_x, x_quad)
+            )
+            self.test_y = self.func(self.test_x)
         return self.test_x, self.test_y
 
-    def get_int_matrix(self, size, training):
-        def get_quad_weights(x):
-            return self.quad_w * x / 2
-
-        if training:
-            self.train_next_batch()
-            x = self.train_x
-        else:
-            self.test()
-            x = self.test_x
-        int_mat = np.zeros((size, x.size), dtype=config.real(np))
-        for i in range(size):
-            int_mat[
-                i, size + self.quad_deg * i : size + self.quad_deg * (i + 1)
-            ] = get_quad_weights(x[i, 0])
-        return int_mat
-
-    def gen_data(self, size):
+    def quad_points(self, X):
         def get_quad_points(x):
             return (self.quad_x + 1) * x / 2
 
-        x = self.geom.uniform_points(size, True)
-        quad_x = np.hstack(map(lambda xi: get_quad_points(xi[0]), x))
-        x = np.vstack((x, quad_x[:, None]))
-        return x, self.func(x)
+        return np.hstack(list(map(lambda xi: get_quad_points(xi[0]), X)))[:, None]
+
+    def get_int_matrix(self, training):
+        def get_quad_weights(x):
+            return self.quad_w * x / 2
+
+        if training or self.num_test is None:
+            size = self.num_domain + self.num_boundary
+            if self.anchors is not None:
+                size += len(self.anchors)
+            x = self.train_x
+        else:
+            size = self.num_test
+            x = self.test_x
+        num_bc = sum(self.num_bcs)
+        int_mat = np.zeros((size + num_bc, x.size), dtype=config.real(np))
+        for i in range(size):
+            beg = size + num_bc + self.quad_deg * i
+            end = beg + self.quad_deg
+            int_mat[i + num_bc, beg:end] = get_quad_weights(x[i + num_bc, 0])
+        return int_mat
