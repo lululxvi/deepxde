d1e42aecde548dae991c9d9e7682257b666bfd00
Lu Lu <lululxvi@gmail.com>
2019-07-30 10:32:33 -0400

    Add metric: APE SD

--- deepxde/metrics.py
+++ deepxde/metrics.py
@@ -17,11 +17,16 @@ def mean_absolute_percentage_error(y_true, y_pred):
     return 100 * np.mean(np.abs(y_true - y_pred) / y_true)
 
 
+def absolute_percentage_error_std(y_true, y_pred):
+    return 100 * np.std(np.abs(y_true - y_pred) / y_true)
+
+
 def get(identifier):
     metric_identifier = {
         "accuracy": accuracy,
         "l2 relative error": l2_relative_error,
         "MAPE": mean_absolute_percentage_error,
+        "APE SD": absolute_percentage_error_std,
     }
 
     if isinstance(identifier, str):
