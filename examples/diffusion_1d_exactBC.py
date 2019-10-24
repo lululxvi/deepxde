e2719cf7be10250fbdff724e98c5c5ec32d6ec5b
lululxvi <erlululu@gmail.com>
2019-10-23 23:09:25 -0400

    Add example of solution to satisfy BC exactly

---
+++ examples/diffusion_1d_exactBC.py
@@ -0,0 +1,59 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+
+import deepxde as dde
+
+
+def main():
+    def pde(x, y):
+        dy_x = tf.gradients(y, x)[0]
+        dy_x, dy_t = dy_x[:, 0], dy_x[:, 1]
+        dy_xx = tf.gradients(dy_x, x)[0][:, 0]
+        return (
+            dy_t
+            - dy_xx
+            + tf.exp(-x[:, 1])
+            * (tf.sin(np.pi * x[:, 0]) - np.pi ** 2 * tf.sin(np.pi * x[:, 0]))
+        )
+
+    def func(x):
+        return np.sin(np.pi * x[:, 0:1]) * np.exp(-x[:, 1:])
+
+    geom = dde.geometry.Interval(-1, 1)
+    timedomain = dde.geometry.TimeDomain(0, 1)
+    geomtime = dde.geometry.GeometryXTime(geom, timedomain)
+
+    bc = dde.DirichletBC(geomtime, func, lambda _, on_boundary: on_boundary)
+    ic = dde.IC(geomtime, func, lambda _, on_initial: on_initial)
+    data = dde.data.TimePDE(
+        geomtime,
+        1,
+        pde,
+        [bc, ic],
+        num_domain=40,
+        func=func,
+        num_test=10000,
+    )
+
+    layer_size = [2] + [32] * 3 + [1]
+    activation = "tanh"
+    initializer = "Glorot uniform"
+    net = dde.maps.FNN(layer_size, activation, initializer)
+    net.outputs_modify(
+        lambda x, y: x[:, 1:2] * (1 - x[:, 0:1] ** 2) * y + tf.sin(np.pi * x[:, 0:1])
+    )
+
+    model = dde.Model(data, net)
+
+    model.compile("adam", lr=0.001, metrics=["l2 relative error"])
+    losshistory, train_state = model.train(epochs=10000)
+
+    dde.saveplot(losshistory, train_state, issave=True, isplot=True)
+
+
+if __name__ == "__main__":
+    main()
