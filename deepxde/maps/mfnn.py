3f6b19299cf42779a503a9743e5f63ec12bf51ae
Lu Lu <lululxvi@gmail.com>
2020-01-19 21:02:19 -0500

    MFNN supports train only high or low fidelity network

--- deepxde/maps/mfnn.py
+++ deepxde/maps/mfnn.py
@@ -24,6 +24,8 @@ class MfNN(Map):
         kernel_initializer,
         regularization=None,
         residue=False,
+        trainable_low_fidelity=True,
+        trainable_high_fidelity=True,
     ):
         self.layer_size_lo = layer_size_low_fidelity
         self.layer_size_hi = layer_size_high_fidelity
@@ -31,6 +33,8 @@ class MfNN(Map):
         self.kernel_initializer = initializers.get(kernel_initializer)
         self.regularizer = regularizers.get(regularization)
         self.residue = residue
+        self.trainable_lo = trainable_low_fidelity
+        self.trainable_hi = trainable_high_fidelity
 
         super(MfNN, self).__init__()
 
@@ -59,13 +63,19 @@ class MfNN(Map):
                 self.layer_size_lo[i + 1],
                 activation=self.activation,
                 regularizer=self.regularizer,
+                trainable=self.trainable_lo,
             )
-        self.y_lo = self.dense(y, self.layer_size_lo[-1], regularizer=self.regularizer)
+        self.y_lo = self.dense(
+            y,
+            self.layer_size_lo[-1],
+            regularizer=self.regularizer,
+            trainable=self.trainable_lo,
+        )
 
         # High fidelity
         X_hi = tf.concat([self.X, self.y_lo], 1)
         # Linear
-        y_hi_l = self.dense(X_hi, self.layer_size_hi[-1])
+        y_hi_l = self.dense(X_hi, self.layer_size_hi[-1], trainable=self.trainable_hi)
         # Nonlinear
         y = X_hi
         for i in range(len(self.layer_size_hi) - 1):
@@ -74,26 +84,39 @@ class MfNN(Map):
                 self.layer_size_hi[i],
                 activation=self.activation,
                 regularizer=self.regularizer,
+                trainable=self.trainable_hi,
             )
         y_hi_nl = self.dense(
-            y, self.layer_size_hi[-1], use_bias=False, regularizer=self.regularizer
+            y,
+            self.layer_size_hi[-1],
+            use_bias=False,
+            regularizer=self.regularizer,
+            trainable=self.trainable_hi,
         )
         # Linear + nonlinear
         if not self.residue:
-            alpha = tf.Variable(0, dtype=config.real(tf))
+            alpha = tf.Variable(0, dtype=config.real(tf), trainable=self.trainable_hi)
             alpha = activations.get("tanh")(alpha)
             self.y_hi = y_hi_l + alpha * y_hi_nl
         else:
-            alpha1 = tf.Variable(0, dtype=config.real(tf))
+            alpha1 = tf.Variable(0, dtype=config.real(tf), trainable=self.trainable_hi)
             alpha1 = activations.get("tanh")(alpha1)
-            alpha2 = tf.Variable(0, dtype=config.real(tf))
+            alpha2 = tf.Variable(0, dtype=config.real(tf), trainable=self.trainable_hi)
             alpha2 = activations.get("tanh")(alpha2)
             self.y_hi = self.y_lo + 0.1 * (alpha1 * y_hi_l + alpha2 * y_hi_nl)
 
         self.target_lo = tf.placeholder(config.real(tf), [None, self.layer_size_lo[-1]])
         self.target_hi = tf.placeholder(config.real(tf), [None, self.layer_size_hi[-1]])
 
-    def dense(self, inputs, units, activation=None, use_bias=True, regularizer=None):
+    def dense(
+        self,
+        inputs,
+        units,
+        activation=None,
+        use_bias=True,
+        regularizer=None,
+        trainable=True,
+    ):
         return tf.layers.dense(
             inputs,
             units,
@@ -101,4 +124,5 @@ class MfNN(Map):
             use_bias=use_bias,
             kernel_initializer=self.kernel_initializer,
             kernel_regularizer=regularizer,
+            trainable=trainable,
         )
