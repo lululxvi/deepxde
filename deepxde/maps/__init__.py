6e648e17da8359cde2f4bafb9f40ca56634ced20
Shunyuan Mao <symao@uvic.ca>
2020-07-17 18:27:41 -0700

    add PFNN to __init__.py

--- deepxde/maps/__init__.py
+++ deepxde/maps/__init__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from .fnn import FNN
 from .mfnn import MfNN
 from .opnn import OpNN
+from .pfnn import PFNN
 from .resnet import ResNet
 
-
-__all__ = ["FNN", "MfNN", "OpNN", "ResNet"]
+__all__ = ["FNN", "MfNN", "OpNN", "PFNN", "ResNet"]
