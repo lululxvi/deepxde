07966b756e0ea986479c3918ff81ff0a0499ce9a
Shunyuan Mao <symao@uvic.ca>
2020-07-17 17:19:31 -0700

    fixed bug about the type of layer_size[-1], improved readblity

--- deepxde/maps/pfnn.py
+++ deepxde/maps/pfnn.py
@@ -49,19 +49,19 @@ class PFNN(Map):
     @timing
     def build(self):
 
-        def layer_map(y, layer_size, net):
+        def layer_map(_y, layer_size, net):
             if net.batch_normalization is None:
-                y = net.dense(y, layer_size, activation=net.activation)
+                _y = net.dense(_y, layer_size, activation=net.activation)
             elif net.batch_normalization == "before":
-                y = net.dense_batchnorm_v1(y, layer_size)
+                _y = net.dense_batchnorm_v1(_y, layer_size)
             elif net.batch_normalization == "after":
-                y = net.dense_batchnorm_v2(y, layer_size)
+                _y = net.dense_batchnorm_v2(_y, layer_size)
             else:
                 raise ValueError("batch_normalization")
             if net.dropout_rate > 0:
-                y = tf.layers.dropout(y, rate=net.dropout_rate, training=net.dropout)
+                _y = tf.layers.dropout(_y, rate=net.dropout_rate, training=net.dropout)
 
-            return y
+            return _y
 
         print("Building feed-forward neural network...")
         self.x = tf.placeholder(config.real(tf), [None, self.layer_size[0]])
@@ -72,7 +72,7 @@ class PFNN(Map):
         # hidden layers
         for i_layer in range(len(self.layer_size) - 2):
             if type(self.layer_size[i_layer + 1]) is list:
-                if type(self.layer_size[i_layer]) is list:
+                if type(y) is list:
                     # e.g. [8, 8, 8] -> [16, 16, 16]
                     assert len(self.layer_size[i_layer + 1]) == len(self.layer_size[i_layer])
                     y = [
@@ -80,7 +80,7 @@ class PFNN(Map):
                         for i_net in range(len(self.layer_size[i_layer + 1]))
                     ]
                 else:
-                    # e.g. 64 -> [16, 16, 16]
+                    # e.g. 64 -> [8, 8, 8]
                     y = [
                         layer_map(y, self.layer_size[i_layer + 1][i_net], self)
                         for i_net in range(len(self.layer_size[i_layer + 1]))
@@ -89,9 +89,9 @@ class PFNN(Map):
                 # e.g. 64 -> 64
                 y = layer_map(y, self.layer_size[i_layer + 1], self)
         # output layers
-        if type(self.layer_size[-1]) is list:
-            # e.g. [3, 3, 3] -> [1, 1, 1]
-            y = [self.dense(y[i_net], self.layer_size[-1][i_net]) for i_net in range(len(self.layer_size[-1]))]
+        if type(y) is list:
+            # e.g. [3, 3, 3] -> [3]
+            y = [self.dense(y[i_net], 1) for i_net in range(len(y))]
             self.y = tf.concat(y, axis=1)
         else:
             self.y = self.dense(y, self.layer_size[-1])
