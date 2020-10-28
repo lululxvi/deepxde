98ba403c58c85b7e1e64ac90201f37c49f09b7e4
Lu Lu <lululxvi@gmail.com>
2020-10-28 00:49:48 -0400

    Bump to v0.8.6

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = "Lu Lu"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "0.8.5"
+release = "0.8.6"
 
 
 # -- General configuration ---------------------------------------------------
