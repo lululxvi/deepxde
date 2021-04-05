b5bc465fedf12ab66c501aa88deccc13a0c921a2
Shunyuan Mao <54871380+smao-astro@users.noreply.github.com>
2021-04-05 20:31:59 +0800

    bc.on_boundary should return N*1 array

--- deepxde/boundary_conditions.py
+++ deepxde/boundary_conditions.py
@@ -21,7 +21,7 @@ class BC(object):
     def __init__(self, geom, on_boundary, component):
         self.geom = geom
         self.on_boundary = lambda x, on: np.array(
-            [on_boundary(x[i], on[i, 0]) for i in range(len(x))]
+            [[on_boundary(x[i], on[i, 0])] for i in range(len(x))]
         )
         self.component = component
 
