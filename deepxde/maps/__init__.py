b6e7a055d1b64174a31908f6500b3f9e9727b166
smao-astro <54871380+smao-astro@users.noreply.github.com>
2021-05-26 09:04:37 +0800

    Add Multi-scale Fourier Feature Neural Networks (#294)

--- deepxde/maps/__init__.py
+++ deepxde/maps/__init__.py
@@ -2,11 +2,22 @@ from __future__ import absolute_import
 
 from .bionet import BiONet
 from .fnn import FNN
+from .msffn import MsFFN
+from .msffn import STMsFFN
 from .mfnn import MfNN
 from .mfonet import MfONet
 from .opnn import OpNN
 from .pfnn import PFNN
 from .resnet import ResNet
 
-
-__all__ = ["BiONet", "FNN", "MfNN", "MfONet", "OpNN", "PFNN", "ResNet"]
+__all__ = [
+    "BiONet",
+    "FNN",
+    "MsFFN",
+    "STMsFFN",
+    "MfNN",
+    "MfONet",
+    "OpNN",
+    "PFNN",
+    "ResNet",
+]
