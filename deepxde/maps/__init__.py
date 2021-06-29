10c7c6e0de610ee560089eb11e01496b84383721
Lu Lu <lululxvi@gmail.com>
2021-06-29 19:27:41 -0400

    Add FourierDeepONetCartesianProd

--- deepxde/maps/__init__.py
+++ deepxde/maps/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
 from .bionet import BiONet
-from .deeponet import DeepONet, DeepONetCartesianProd
+from .deeponet import DeepONet, DeepONetCartesianProd, FourierDeepONetCartesianProd
 from .fnn import FNN
 from .msffn import MsFFN
 from .msffn import STMsFFN
@@ -14,6 +14,7 @@ __all__ = [
     "BiONet",
     "DeepONet",
     "DeepONetCartesianProd",
+    "FourierDeepONetCartesianProd",
     "FNN",
     "MsFFN",
     "STMsFFN",
