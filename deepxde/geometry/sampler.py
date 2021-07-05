73864e5bca7c53b33d03560e1e7ec73951274d63
Lu Lu <lululxvi@gmail.com>
2021-07-04 23:15:47 -0400

    Support skopt>=0.9 with different sampler.Sobol

--- deepxde/geometry/sampler.py
+++ deepxde/geometry/sampler.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from distutils.version import LooseVersion
+
 import numpy as np
 import skopt
 
@@ -12,8 +14,9 @@ def sample(n_samples, dimension, sampler="pseudo"):
     Args:
         n_samples (int): The number of samples.
         dimension (int): Space dimension.
-        sampler (string): One of the following: "pseudo" (pseudorandom), "LHS" (Latin hypercube sampling), "Halton"
-            (Halton sequence), "Hammersley" (Hammersley sequence), or "Sobol" (Sobol sequence).
+        sampler (string): One of the following: "pseudo" (pseudorandom), "LHS" (Latin
+            hypercube sampling), "Halton" (Halton sequence), "Hammersley" (Hammersley
+            sequence), or "Sobol" (Sobol sequence).
     """
     if sampler == "pseudo":
         return pseudo(n_samples, dimension)
@@ -38,8 +41,13 @@ def quasirandom(n_samples, dimension, sampler):
     elif sampler == "Hammersley":
         sampler = skopt.sampler.Hammersly(min_skip=-1, max_skip=-1)
     elif sampler == "Sobol":
-        # Remove the first point [0, 0, ...] and the second point [0.5, 0.5, ...], which are too special and may cause
-        # some error.
-        sampler = skopt.sampler.Sobol(min_skip=2, max_skip=2, randomize=False)
+        # Remove the first point [0, 0, ...] and the second point [0.5, 0.5, ...], which
+        # are too special and may cause some error.
+        if LooseVersion(skopt.__version__) < LooseVersion("0.9"):
+            sampler = skopt.sampler.Sobol(min_skip=2, max_skip=2, randomize=False)
+        else:
+            sampler = skopt.sampler.Sobol(skip=0, randomize=False)
+            space = [(0.0, 1.0)] * dimension
+            return np.array(sampler.generate(space, n_samples + 2)[2:])
     space = [(0.0, 1.0)] * dimension
     return np.array(sampler.generate(space, n_samples))
