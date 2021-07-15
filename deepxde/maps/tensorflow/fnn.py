cee668e28970a155d3ff8094a8447ba61979e314
ZongrenZou <31862613+ZongrenZou@users.noreply.github.com>
2021-07-14 21:15:37 -0400

    tensorflow maps support regularization, input/output transform and property decorator

--- deepxde/maps/tensorflow/fnn.py
+++ deepxde/maps/tensorflow/fnn.py
@@ -5,14 +5,20 @@ from __future__ import print_function
 from .nn import NN
 from .. import activations
 from .. import initializers
+from .. import regularizers
 from ...backend import tf
 
 
 class FNN(NN):
     """Fully-connected neural network."""
 
-    def __init__(self, layer_sizes, activation, kernel_initializer):
+    def __init__(self, 
+                 layer_sizes, 
+                 activation, 
+                 kernel_initializer,
+                 regularization=None):
         super(FNN, self).__init__()
+        self.regularizer = regularizers.get(regularization)
 
         self.denses = []
         activation = activations.get(activation)
@@ -20,14 +26,53 @@ class FNN(NN):
         for units in layer_sizes[1:-1]:
             self.denses.append(
                 tf.keras.layers.Dense(
-                    units, activation=activation, kernel_initializer=initializer
+                    units, 
+                    activation=activation, 
+                    kernel_initializer=initializer,
+                    kernel_regularizer=self.regularizer
                 )
             )
         self.denses.append(
-            tf.keras.layers.Dense(layer_sizes[-1], kernel_initializer=initializer)
+            tf.keras.layers.Dense(
+                layer_sizes[-1], 
+                kernel_initializer=initializer,
+                kernel_regularizer=self.regularizer
+            )
         )
+        self._inputs = None
+        self._targets = None
+        self._data_id = None
 
     def call(self, inputs, training=False):
+        y = inputs
+        if self._input_transform is not None:
+            y = self._input_transform(y)
         for f in self.denses:
-            inputs = f(inputs)
-        return inputs
+            y = f(y)
+        if self._output_transform is not None:
+            y = self._output_transform(inputs, y) 
+        return y
+
+    @property
+    def inputs(self):
+        return self._inputs
+    
+    @inputs.setter
+    def inputs(self, value):
+        self._inputs = value
+    
+    @property
+    def targets(self):
+        return self._targets
+    
+    @targets.setter
+    def targets(self, value):
+        self._targets = value
+    
+    @property
+    def data_id(self):
+        return self._data_id
+    
+    @data_id.setter
+    def data_id(self, value):
+        self._data_id = value
