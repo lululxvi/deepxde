d20a9977e3c238be5bde3f0d5f611a2ac643b2eb
Lu Lu <lululxvi@gmail.com>
2021-07-28 01:39:08 -0400

    Backend PyTorch supports Adam optimizer

--- deepxde/optimizers/pytorch/optimizers.py
+++ deepxde/optimizers/pytorch/optimizers.py
@@ -1,5 +1,21 @@
-__all__ = ["is_external_optimizer"]
+import torch
+
+
+__all__ = ["get", "is_external_optimizer"]
 
 
 def is_external_optimizer(optimizer):
     return False
+
+
+def get(params, optimizer, learning_rate=None, decay=None):
+    """Retrieves an Optimizer instance."""
+    if isinstance(optimizer, torch.optim.Optimizer):
+        return optimizer
+    if learning_rate is None:
+        raise ValueError("No learning rate for {}.".format(optimizer))
+
+    # TODO: decay
+    if optimizer == "adam":
+        return torch.optim.Adam(params, lr=learning_rate)
+    raise NotImplementedError(f"{optimizer} to be implemented for backend pytorch.")
