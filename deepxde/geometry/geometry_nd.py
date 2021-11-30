f0f38a14895d2bff6146f76c134bc5a2f5b08d05
Olivier Claessen <olivier.claessen@ru.nl>
2021-11-30 11:24:49 +0100

    fixed type runtime error pytorch test set

--- deepxde/geometry/geometry_nd.py
+++ deepxde/geometry/geometry_nd.py
@@ -68,7 +68,7 @@ class Hypercube(Geometry):
                         1:
                     ]
                 )
-        x = np.array(list(itertools.product(*xi)))
+        x = np.array(list(itertools.product(*xi)), dtype=config.real(np))
         if n != len(x):
             print(
                 "Warning: {} points required, but {} points sampled.".format(n, len(x))
