f750d621817577dee617f3d2e280b175d8dda349
ShuaiMeng0601 <79709874+ShuaiMeng0601@users.noreply.github.com>
2021-12-23 15:44:19 -0500

    Update triple.py

--- deepxde/data/triple.py
+++ deepxde/data/triple.py
@@ -1,9 +1,6 @@
 from .data import Data
 from .sampler import BatchSampler
-import abc
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+
 
 
 class Triple(Data):
