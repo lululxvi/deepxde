88b41632378e28d4e663ca464baeac2691b261c5
ShuaiMeng0601 <79709874+ShuaiMeng0601@users.noreply.github.com>
2021-12-23 15:42:06 -0500

    Update triple.py
    Change the name and delete import code.
    Add the if commands ("len(X_train[0]) != len(X_train[1])") for the case that the sensor number of input1 is not equal to the sensor number of input2; add the comments as well

--- deepxde/data/triple.py
+++ deepxde/data/triple.py
@@ -86,22 +86,30 @@ class TripleCartesianProd(Data):
         return self.test_x, self.test_y
     
     
-class BiONet_TripleCartesianProd(Data):
+class QuadrupleCartesianProd(Data):
     """Cartesian Product input data format for BioNet architecture.
     This dataset can be used with the network ``BiONetCartesianProd`` for operator
     learning.
     Args:
-        X_train: A tuple of three NumPy arrays. The first two elements have the shape (`N1`,
-            `dim1`), and the third element has the shape (`N2`, `dim2`). 
+        X_train: A tuple of three NumPy arrays. The first element has the shape (`N1`,
+            `dim1`), the second element has the shape (`N1`, `dim2`), and the third element has the shape (`N2`, `dim3`).
         y_train: A NumPy array of shape (`N1`, `N2`).
     """
 
     def __init__(self, X_train, y_train, X_test, y_test):
-        if len(X_train[0]) * len(X_train[2]) != y_train.size or len(X_train[1]) * len(X_train[2]) != y_train.size:
+        if (
+            len(X_train[0]) * len(X_train[2]) != y_train.size
+            or len(X_train[1]) * len(X_train[2]) != y_train.size
+            or len(X_train[0]) != len(X_train[1])
+        ):
             raise ValueError(
                 "The training dataset does not have the format of Cartesian product."
             )
-        if len(X_test[0]) * len(X_test[2]) != y_test.size or len(X_test[1]) * len(X_test[2]) != y_test.size:
+        if (
+            len(X_test[0]) * len(X_test[2]) != y_test.size
+            or len(X_test[1]) * len(X_test[2]) != y_test.size
+            or len(X_test[0]) != len(X_test[1])
+        ):
             raise ValueError(
                 "The testing dataset does not have the format of Cartesian product."
             )
@@ -117,7 +125,11 @@ class BiONet_TripleCartesianProd(Data):
         if batch_size is None:
             return self.train_x, self.train_y
         indices = self.train_sampler.get_next(batch_size)
-        return (self.train_x[0][indices], self.train_x[1][indices], self.train_x[2]), self.train_y[indices]
+        return (
+            self.train_x[0][indices],
+            self.train_x[1][indices],
+            self.train_x[2],
+        ), self.train_y[indices]
 
     def test(self):
         return self.test_x, self.test_y
