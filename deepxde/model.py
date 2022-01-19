9595979bf246c232dcecb546d6a2fbb976491f46
pescap <pescapil@uc.cl>
2022-01-19 11:46:10 -0800

    stop training if nan

--- deepxde/model.py
+++ deepxde/model.py
@@ -384,6 +384,9 @@ class Model(object):
             self.train_state.epoch += 1
             self.train_state.step += 1
             if self.train_state.step % display_every == 0 or i + 1 == epochs:
+                if (np.isnan(self.train_state.loss_train).any()) or (np.isnan(self.train_state.loss_test).any()):
+                    self.stop_training = True
+                    break
                 self._test()
 
             self.callbacks.on_batch_end()
