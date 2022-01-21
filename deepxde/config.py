885689055c0ae16a2bc9ac128b429ccf6cc3b7c2
pescapil <paulescapil@gmail.com>
2022-01-21 09:24:57 -0300

    Update config.py
    set_random_seed rather than set_seed

--- deepxde/config.py
+++ deepxde/config.py
@@ -34,7 +34,7 @@ def set_default_float(value):
     tf.keras.backend.set_floatx(value)
 
 
-def set_seed(seed):
+def set_random_seed(seed):
     """Set the global random seed.
 
     For reproductibility purposes, one has to set the random, numpy and backend seeds.
