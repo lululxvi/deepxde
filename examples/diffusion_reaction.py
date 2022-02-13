b7eecc0041bc29bf37dc6f3c493f0a539b20d1b2
bfan <bfanmbt05@gmail.com>
2022-02-12 23:06:02 -0800

    created diffusion_reaction.py

---
+++ examples/diffusion_reaction.py
@@ -0,0 +1,55 @@
+"""Backend supported: tensorflow.compat.v1, tensorflow, pytorch"""
+import deepxde as dde
+import numpy as np
+# Backend tensorflow.compat.v1 or tensorflow
+from deepxde.backend import tf
+# Backend pytorch
+# import torch
+
+
+def pde(x, y):
+    dy_t = dde.grad.jacobian(y, x, i=0, j=1)
+    dy_xx = dde.grad.hessian(y, x, i=0, j=0)
+    # Backend tensorflow.compat.v1 or tensorflow
+    return (
+        dy_t
+        - dy_xx
+        + tf.exp(-x[:, 1:])
+        * (tf.sin(np.pi * x[:, 0:1]) - np.pi ** 2 * tf.sin(np.pi * x[:, 0:1]))
+    )
+    # Backend pytorch
+    # return (
+    #     dy_t
+    #     - dy_xx
+    #     + torch.exp(-x[:, 1:])
+    #     * (torch.sin(np.pi * x[:, 0:1]) - np.pi ** 2 * torch.sin(np.pi * x[:, 0:1]))
+    # )
+
+
+def func(x):
+    return np.sin(np.pi * x[:, 0:1]) * np.exp(-x[:, 1:])
+
+
+geom = dde.geometry.Interval(-1, 1)
+timedomain = dde.geometry.TimeDomain(0, 1)
+geomtime = dde.geometry.GeometryXTime(geom, timedomain)
+
+data = dde.data.TimePDE(geomtime, pde, [], num_domain=40, solution=func, num_test=10000)
+
+layer_size = [2] + [32] * 3 + [1]
+activation = "tanh"
+initializer = "Glorot uniform"
+net = dde.maps.FNN(layer_size, activation, initializer)
+net.apply_output_transform(
+    # Backend tensorflow.compat.v1 or tensorflow
+    lambda x, y: x[:, 1:2] * (1 - x[:, 0:1] ** 2) * y + tf.sin(np.pi * x[:, 0:1])
+    # Backend pytorch
+    # lambda x, y: x[:, 1:2] * (1 - x[:, 0:1] ** 2) * y + torch.sin(np.pi * x[:, 0:1])
+)
+
+model = dde.Model(data, net)
+
+model.compile("adam", lr=0.001, metrics=["l2 relative error"])
+losshistory, train_state = model.train(epochs=10000)
+
+dde.saveplot(losshistory, train_state, issave=True, isplot=True)
