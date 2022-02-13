2efa7ef10ab79da85067d91986d0292fb07f78ec
bfan <bfanmbt05@gmail.com>
2022-02-12 23:07:50 -0800

    edited diffusion_reaction.py

--- examples/diffusion_reaction.py
+++ examples/diffusion_reaction.py
@@ -1,55 +1,81 @@
-"""Backend supported: tensorflow.compat.v1, tensorflow, pytorch"""
 import deepxde as dde
 import numpy as np
-# Backend tensorflow.compat.v1 or tensorflow
+ 
+# Backend tensorflow.compact.v1 or tensorflow
 from deepxde.backend import tf
+
 # Backend pytorch
 # import torch
-
-
+ 
 def pde(x, y):
-    dy_t = dde.grad.jacobian(y, x, i=0, j=1)
-    dy_xx = dde.grad.hessian(y, x, i=0, j=0)
-    # Backend tensorflow.compat.v1 or tensorflow
-    return (
-        dy_t
-        - dy_xx
-        + tf.exp(-x[:, 1:])
-        * (tf.sin(np.pi * x[:, 0:1]) - np.pi ** 2 * tf.sin(np.pi * x[:, 0:1]))
-    )
-    # Backend pytorch
-    # return (
-    #     dy_t
-    #     - dy_xx
-    #     + torch.exp(-x[:, 1:])
-    #     * (torch.sin(np.pi * x[:, 0:1]) - np.pi ** 2 * torch.sin(np.pi * x[:, 0:1]))
-    # )
-
-
+  dy_t = dde.grad.jacobian(y, x, i=0, j=1)
+  dy_xx = dde.grad.hessian(y, x, i=0, j=0)
+  d = 1
+  return (
+      dy_t
+      - d * dy_xx
+      - tf.exp(-x[:, 1:])
+      * (3 * tf.sin(2 * x[:, 0:1]) / 2
+         + 8 * tf.sin(3 * x[:, 0:1]) / 3
+         + 15 * tf.sin(4 * x[:, 0:1]) / 4
+         + 63 * tf.sin(8 * x[:, 0:1]) / 8
+      )
+  )
+  # Backend pytorch
+  # return (
+  #     dy_t
+  #     - d * dy_xx
+  #     - torch.exp(-x[:, 1:])
+  #     * (3 * torch.sin(2 * x[:, 0:1]) / 2
+  #        + 8 * torch.sin(3 * x[:, 0:1]) / 3
+  #        + 15 * torch.sin(4 * x[:, 0:1]) / 4
+  #        + 63 * torch.sin(8 * x[:, 0:1]) / 8
+  #     )
+  # )
+ 
 def func(x):
-    return np.sin(np.pi * x[:, 0:1]) * np.exp(-x[:, 1:])
-
-
-geom = dde.geometry.Interval(-1, 1)
+  return (
+      np.exp(-x[:, 1:]) 
+      * (np.sin(x[:, 0:1])
+         + np.sin(2 * x[:, 0:1]) / 2
+         + np.sin(3 * x[:, 0:1]) / 3
+         + np.sin(4 * x[:, 0:1]) / 4
+         + np.sin(8 * x[:, 0:1]) / 8
+      )
+  )
+ 
+geom = dde.geometry.Interval(-np.pi, np.pi)
 timedomain = dde.geometry.TimeDomain(0, 1)
 geomtime = dde.geometry.GeometryXTime(geom, timedomain)
-
-data = dde.data.TimePDE(geomtime, pde, [], num_domain=40, solution=func, num_test=10000)
-
-layer_size = [2] + [32] * 3 + [1]
+ 
+data = dde.data.TimePDE(geomtime, pde, [], num_domain=320, solution=func, num_test=80000)
+ 
+layer_size = [2] + [30] * 6 + [1]
 activation = "tanh"
 initializer = "Glorot uniform"
 net = dde.maps.FNN(layer_size, activation, initializer)
+ 
 net.apply_output_transform(
-    # Backend tensorflow.compat.v1 or tensorflow
-    lambda x, y: x[:, 1:2] * (1 - x[:, 0:1] ** 2) * y + tf.sin(np.pi * x[:, 0:1])
+    # Backend tensorflow.compact.v1 or tensorflow
+    lambda x, y: x[:, 1:2] * (np.pi ** 2 - x[:, 0:1] ** 2) * y
+    + tf.sin(x[:, 0:1])
+    + tf.sin(2 * x[:, 0:1]) / 2
+    + tf.sin(3 * x[:, 0:1]) / 3
+    + tf.sin(4 * x[:, 0:1]) / 4
+    + tf.sin(8 * x[:, 0:1]) / 8
+
     # Backend pytorch
-    # lambda x, y: x[:, 1:2] * (1 - x[:, 0:1] ** 2) * y + torch.sin(np.pi * x[:, 0:1])
+    # lambda x, y: x[:, 1:2] * (np.pi ** 2 - x[:, 0:1] ** 2) * y
+    # + torch.sin(x[:, 0:1])
+    # + torch.sin(2 * x[:, 0:1]) / 2
+    # + torch.sin(3 * x[:, 0:1]) / 3
+    # + torch.sin(4 * x[:, 0:1]) / 4
+    # + torch.sin(8 * x[:, 0:1]) / 8
+
 )
-
+ 
 model = dde.Model(data, net)
-
-model.compile("adam", lr=0.001, metrics=["l2 relative error"])
-losshistory, train_state = model.train(epochs=10000)
-
+model.compile("adam", lr=1e-3, metrics=["l2 relative error"])
+losshistory, train_state = model.train(epochs=20000)
+ 
 dde.saveplot(losshistory, train_state, issave=True, isplot=True)
