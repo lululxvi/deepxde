f19c628ce30d574c19ef3528f037716686174cfa
ShuaiMeng0601 <79709874+ShuaiMeng0601@users.noreply.github.com>
2022-02-15 16:26:17 -0500

    Update and rename bionet.py to mionet.py
    1. change the name to mionet
    2. add the low rank version of mionet in MIONetCartesianProd class
    3. the activation functions of branch net can be defined separately:
    for example: {'branch1':relu, 'branch2':None, 'trunk':'relu'}

--- deepxde/nn/tensorflow_compat_v1/mionet.py
+++ deepxde/nn/tensorflow_compat_v1/mionet.py
@@ -7,8 +7,8 @@ from ...backend import tf
 from ...utils import timing
 
 
-class BiONet(NN):
-    """Deep operator network with two input functions."""
+class MIONet(NN):
+    """Multiple-input Operator Network with two input functions."""
 
     def __init__(
         self,
@@ -19,16 +19,19 @@ class BiONet(NN):
         kernel_initializer,
         regularization=None,
     ):
-        super(BiONet, self).__init__()
+        super(MIONet, self).__init__()
 
         self.layer_branch1 = layer_sizes_branch1
         self.layer_branch2 = layer_sizes_branch2
         self.layer_trunk = layer_sizes_trunk
         if isinstance(activation, dict):
-            self.activation_branch = activations.get(activation["branch"])
+            self.activation_branch1 = activations.get(activation["branch1"])
+            self.activation_branch2 = activations.get(activation["branch2"])
             self.activation_trunk = activations.get(activation["trunk"])
         else:
-            self.activation_branch = self.activation_trunk = activations.get(activation)
+            self.activation_branch1 = (
+                self.activation_branch2
+            ) = self.activation_trunk = activations.get(activation)
         self.kernel_initializer = initializers.get(kernel_initializer)
         self.regularizer = regularizers.get(regularization)
 
@@ -48,16 +51,16 @@ class BiONet(NN):
 
     @timing
     def build(self):
-        print("Building BiONet...")
+        print("Building MIONet...")
         self.X_func1 = tf.placeholder(config.real(tf), [None, self.layer_branch1[0]])
         self.X_func2 = tf.placeholder(config.real(tf), [None, self.layer_branch2[0]])
         self.X_loc = tf.placeholder(config.real(tf), [None, self.layer_trunk[0]])
         self._inputs = [self.X_func1, self.X_func2, self.X_loc]
 
         # Branch net 1
-        y_func1 = self._branch_net(self.X_func1, self.layer_branch1[1:])
+        y_func1 = self._branch_net1(self.X_func1, self.layer_branch1[1:])
         # Branch net 2
-        y_func2 = self._branch_net(self.X_func2, self.layer_branch2[1:])
+        y_func2 = self._branch_net2(self.X_func2, self.layer_branch2[1:])
         # Trunk net
         y_loc = self._trunk_net(self.X_loc, self.layer_trunk[1:])
 
@@ -72,16 +75,27 @@ class BiONet(NN):
         self.target = tf.placeholder(config.real(tf), [None, 1])
         self.built = True
 
-    def _branch_net(self, X_func, layer_branch):
+    def _branch_net1(self, X_func, layer_branch):
         y_func = X_func
-        for i in range(len(layer_branch)):
+        for i in range(len(layer_branch) - 1):
             y_func = self._dense(
                 y_func,
                 layer_branch[i],
-                activation=self.activation_branch,
+                activation=self.activation_branch1,
                 regularizer=self.regularizer,
             )
-        return y_func
+        return self._dense(y_func, layer_branch[-1], regularizer=self.regularizer)
+
+    def _branch_net2(self, X_func, layer_branch):
+        y_func = X_func
+        for i in range(len(layer_branch) - 1):
+            y_func = self._dense(
+                y_func,
+                layer_branch[i],
+                activation=self.activation_branch2,
+                regularizer=self.regularizer,
+            )
+        return self._dense(y_func, layer_branch[-1], regularizer=self.regularizer)
 
     def _trunk_net(self, X_loc, layer_trunk):
         y_loc = X_loc
@@ -112,14 +126,14 @@ class BiONet(NN):
             kernel_regularizer=regularizer,
             trainable=trainable,
         )
-    
-    
-class BiONetCartesianProd(BiONet):
-    """Deep operator network with two input functions for Cartesian product format dataset."""
+
+
+class MIONetCartesianProd(MIONet):
+    """MIONet with two input functions for Cartesian product format."""
 
     @timing
     def build(self):
-        print("Building BiONetCartesianProd...")
+        print("Building MIONetCartesianProd...")
 
         self.X_func1 = tf.placeholder(config.real(tf), [None, self.layer_branch1[0]])
         self.X_func2 = tf.placeholder(config.real(tf), [None, self.layer_branch2[0]])
@@ -127,25 +141,17 @@ class BiONetCartesianProd(BiONet):
         self._inputs = [self.X_func1, self.X_func2, self.X_loc]
 
         # Branch net 1
-        y_func1 = self._branch_net(self.X_func1, self.layer_branch1[1:])
+        y_func1 = self._branch_net1(self.X_func1, self.layer_branch1[1:])
         # Branch net 2
-        y_func2 = self._branch_net(self.X_func2, self.layer_branch2[1:])
+        y_func2 = self._branch_net2(self.X_func2, self.layer_branch2[1:])
         # Trunk net
         y_loc = self._trunk_net(self.X_loc, self.layer_trunk[1:])
 
         # Dot product
-        y_loc = tf.reshape(y_loc, (-1, self.layer_branch1[-1], self.layer_branch2[-1]))
-        # self.y = tf.einsum("bji,bi->bj", y_loc, y_func2)
-        # self.y = tf.einsum("bi,bi->b", self.y, y_func1)
-        # self.y = tf.expand_dims(self.y, axis=1)
-        self.y = tf.einsum("bj, nij -> bni", y_func1, y_loc)
-        self.y = tf.einsum("bni,bi->bn", self.y, y_func2)
-
-        # self.y = tf.einsum("bi, nij -> bnj", y_func1, y_loc)
-        # self.y = tf.einsum("bnj,bj->bn", self.y, y_func2)
+        self.y = tf.multiply(y_func1, y_func2)
+        self.y = tf.einsum("ip,jp->ij", self.y, y_loc)
 
         b = tf.Variable(tf.zeros(1))
         self.y += b
-
         self.target = tf.placeholder(config.real(tf), [None, None])
         self.built = True
