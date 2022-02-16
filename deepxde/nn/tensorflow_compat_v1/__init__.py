cfb29dab56dc5a5a2fda41da5af57cfc234cabec
ShuaiMeng0601 <79709874+ShuaiMeng0601@users.noreply.github.com>
2022-02-16 14:53:39 -0500

    Update __init__.py

--- deepxde/nn/tensorflow_compat_v1/__init__.py
+++ deepxde/nn/tensorflow_compat_v1/__init__.py
@@ -1,17 +1,15 @@
 """Package for tensorflow.compat.v1 NN modules."""
 
-from .mionet import MIONet, MIONetCartesianProd
 from .deeponet import DeepONet, DeepONetCartesianProd, FourierDeepONetCartesianProd
 from .fnn import FNN, PFNN
 from .mfnn import MfNN
 from .mfonet import MfONet
+from .mionet import MIONet, MIONetCartesianProd
 from .msffn import MsFFN, STMsFFN
 from .nn import NN
 from .resnet import ResNet
 
 __all__ = [
-    "MIONet",
-    "MIONetCartesianProd",
     "DeepONet",
     "DeepONetCartesianProd",
     "FourierDeepONetCartesianProd",
@@ -19,6 +17,8 @@ __all__ = [
     "PFNN",
     "MfNN",
     "MfONet",
+    "MIONet",
+    "MIONetCartesianProd",
     "MsFFN",
     "NN",
     "STMsFFN",
