8287dd4d02fed3043868b0a199e48bce5893b82d
ShuaiMeng0601 <79709874+ShuaiMeng0601@users.noreply.github.com>
2022-02-22 14:46:36 -0500

    Update quadruple.py
    add the quadruple class for the non-cartesian product form

--- deepxde/data/quadruple.py
+++ deepxde/data/quadruple.py
@@ -2,6 +2,41 @@ from .data import Data
 from .sampler import BatchSampler
 
 
+class Quadruple(Data):
+    """Dataset with each data point as a quadruple.
+    The couple of the first three elements are the input, and the fourth element is the
+    output. This dataset can be used with the network ``MIONet`` for operator
+    learning.
+
+    Args:
+        X_train: A tuple of three NumPy arrays.
+        y_train: A NumPy array."""
+
+    def __init__(self, X_train, y_train, X_test, y_test):
+        self.train_x = X_train
+        self.train_y = y_train
+        self.test_x = X_test
+        self.test_y = y_test
+
+        self.train_sampler = BatchSampler(len(self.train_y), shuffle=True)
+
+    def losses(self, targets, outputs, loss, model):
+        return [loss(targets, outputs)]
+
+    def train_next_batch(self, batch_size=None):
+        if batch_size is None:
+            return self.train_x, self.train_y
+        indices = self.train_sampler.get_next(batch_size)
+        return (
+            (self.train_x[0][indices], self.train_x[1][indices]),
+            self.train_x[2][indices],
+            self.train_y[indices],
+        )
+
+    def test(self):
+        return self.test_x, self.test_y
+
+
 class QuadrupleCartesianProd(Data):
     """Cartesian Product input data format for MIONet architecture.
 
