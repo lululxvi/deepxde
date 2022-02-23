d2f6fb73c20f2acb5627215374fb7233142fefc3
bfan <bfanmbt05@gmail.com>
2022-02-22 21:59:56 -0800

    edited brinkman_forchheimer.py

--- examples/pinn_inverse/brinkman_forchheimer.py
+++ examples/pinn_inverse/brinkman_forchheimer.py
@@ -11,15 +11,15 @@ import numpy as np
 g = 1
 v = 1e-3
 e = 0.4
-K = 1e-3
 H = 1
 
 v_e = dde.Variable(0.1)
+K = dde.Variable(0.1)
 
 
 def sol(x):
-    r = (v * e / (1e-3 * K)) ** (0.5)
-    return g * K / v * (1 - np.cosh(r * (x - H / 2)) / np.cosh(r * H / 2))
+    r = (v * e / (1e-3 * 1e-3)) ** (0.5)
+    return g * 1e-3 / v * (1 - np.cosh(r * (x - H / 2)) / np.cosh(r * H / 2))
 
 
 def gen_traindata(num):
@@ -29,9 +29,8 @@ def gen_traindata(num):
 
 
 def pde(x, y):
-    u = y
     du_xx = dde.grad.hessian(y, x)
-    return -v_e / e * du_xx + v * u / K - g
+    return -v_e / e * du_xx + v * y / K - g
 
 
 def output_transform(x, y):
@@ -53,17 +52,17 @@ data = dde.data.PDE(
     num_test=500,
 )
 
-net = dde.maps.FNN([1] + [20] * 3 + [1], "tanh", "Glorot uniform")
+net = dde.nn.FNN([1] + [20] * 3 + [1], "tanh", "Glorot uniform")
 net.apply_output_transform(output_transform)
 model = dde.Model(data, net)
 model.compile("adam", lr=0.001, metrics=["l2 relative error"])
-variable = dde.callbacks.VariableValue([v_e], period=200, filename="variables1.dat")
+variable = dde.callbacks.VariableValue([v_e, K], period=200, filename="variables1.dat")
 
 losshistory, train_state = model.train(epochs=30000, callbacks=[variable])
 dde.saveplot(losshistory, train_state, issave=True, isplot=True)
 
 lines = open("variables1.dat", "r").readlines()
-v_ehat1 = np.array(
+vkinfer = np.array(
     [
         np.fromstring(
             min(re.findall(re.escape("[") + "(.*?)" + re.escape("]"), line), key=len),
@@ -73,21 +72,36 @@ v_ehat1 = np.array(
     ]
 )
 
-l, c = v_ehat1.shape
+l, c = vkinfer.shape
 v_etrue = 1e-3
+ktrue = 1e-3
 
 plt.figure()
 plt.plot(
     range(0, 200 * l, 200),
-    np.ones(v_ehat1[:, 0].shape) * v_etrue,
+    np.ones(vkinfer[:, 0].shape) * v_etrue,
     color="black",
     label="Exact",
 )
-plt.plot(range(0, 200 * l, 200), v_ehat1[:, 0], "b--", label="Pred")
+plt.plot(range(0, 200 * l, 200), vkinfer[:, 0], "b--", label="Pred")
 plt.xlabel("Epoch")
 plt.yscale("log")
 plt.ylim(top=1e-1)
 plt.legend(frameon=False)
 plt.ylabel(r"$\nu_e$")
+
+plt.figure()
+plt.plot(
+    range(0, 200 * l, 200),
+    np.ones(vkinfer[:, 1].shape) * ktrue,
+    color="black",
+    label="Exact",
+)
+plt.plot(range(0, 200 * l, 200), vkinfer[:, 1], "b--", label="Pred")
+plt.xlabel("Epoch")
+plt.yscale("log")
+plt.ylim(ymax=1e-1)
+plt.legend(frameon=False)
+plt.ylabel(r"$K$")
 
 plt.show()
