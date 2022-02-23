0b6c6c09187586b0044950daa1ce804e7a5f4d89
ShuaiMeng0601 <79709874+ShuaiMeng0601@users.noreply.github.com>
2022-02-22 23:35:58 -0500

    Support MIONet: Quadruple, QuadrupleCartesianProd, MIONet, MIONetCartesianProd (#455)

--- deepxde/nn/tensorflow_compat_v1/__init__.py
+++ deepxde/nn/tensorflow_compat_v1/__init__.py
@@ -1,25 +1,26 @@
 """Package for tensorflow.compat.v1 NN modules."""
 
-from .bionet import BiONet
 from .deeponet import DeepONet, DeepONetCartesianProd, FourierDeepONetCartesianProd
 from .fnn import FNN, PFNN
 from .mfnn import MfNN
 from .mfonet import MfONet
+from .mionet import MIONet, MIONetCartesianProd
 from .msffn import MsFFN, STMsFFN
 from .nn import NN
 from .resnet import ResNet
 
 __all__ = [
-    "BiONet",
     "DeepONet",
     "DeepONetCartesianProd",
-    "FourierDeepONetCartesianProd",
     "FNN",
-    "PFNN",
+    "FourierDeepONetCartesianProd",
     "MfNN",
     "MfONet",
+    "MIONet",
+    "MIONetCartesianProd",
     "MsFFN",
     "NN",
-    "STMsFFN",
+    "PFNN",
     "ResNet",
+    "STMsFFN",
 ]
