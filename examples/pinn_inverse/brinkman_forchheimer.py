9d6a27ddd7595a495b8f699c6262ed9beb959f43
bfan <bfanmbt05@gmail.com>
2022-02-22 17:52:31 -0800

    edited example brinkman_forchheimer.py

--- examples/pinn_inverse/brinkman_forchheimer.py
+++ examples/pinn_inverse/brinkman_forchheimer.py
@@ -1,12 +1,11 @@
-"""Backend supported: tensorflow.compat.v1, tensorflow
+"""Backend supported: tensorflow.compat.v1, tensorflow, pytorch
 
 Implementation of Brinkman-Forchheimer equation example in paper https://arxiv.org/pdf/2111.02801.pdf.
 """
 import deepxde as dde
 import matplotlib.pyplot as plt
 import numpy as np
-
-from deepxde.backend import tf
+import re
 
 g = 1
 v = 1e-3
@@ -14,7 +13,7 @@ e = 0.4
 K = 1e-3
 H = 1
 
-v_e = tf.math.softplus(tf.Variable(0, trainable=True, dtype=tf.float32))*0.1
+v_e = dde.Variable(0.1)
 
 def sol(x):    
   r = (v*e/(1e-3*K))**(0.5)
@@ -38,28 +37,27 @@ ob_x, ob_u = gen_traindata(5)
 observe_u = dde.PointSetBC(ob_x, ob_u, component = 0)
 
 data = dde.data.PDE(
-    geom,
-    pde,
-    solution = sol,
-    bcs = [observe_u],
-    num_domain = 100,
-    num_boundary = 50,
-    train_distribution = "uniform",
-    num_test = 500,
-    )
+  geom,
+  pde,
+  solution = sol,
+  bcs = [observe_u],
+  num_domain = 100,
+  num_boundary = 0,
+  train_distribution = "uniform",
+  num_test = 500,
+  )
 
 net = dde.maps.FNN([1] + [20] * 3 + [1], "tanh", "Glorot uniform")
 net.apply_output_transform(output_transform)
 model = dde.Model(data, net)
 model.compile("adam", lr = 0.001, metrics = ["l2 relative error"])
 variable = dde.callbacks.VariableValue(
-    [v_e], period=200, filename="variables1.dat"
+  [v_e], period=200, filename="variables1.dat"
 )
 
 losshistory, train_state = model.train(epochs = 30000, callbacks = [variable])
 dde.saveplot(losshistory, train_state, issave = True, isplot = True)
 
-import re
 lines = open("variables1.dat", "r").readlines()
 v_ehat1 = np.array([np.fromstring(min(re.findall(re.escape('[')+"(.*?)"+re.escape(']'),line), key=len), sep=',') for line in lines])
 
