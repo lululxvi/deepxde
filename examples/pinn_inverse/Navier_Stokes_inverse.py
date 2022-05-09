fc65ec73fb6ef31b360706aa827d70b9d1d31e9d
zyz-rise <95417257+zyz-rise@users.noreply.github.com>
2022-05-09 10:19:58 +0800

    Update Navier_Stokes_inverse.py

--- examples/pinn_inverse/Navier_Stokes_inverse.py
+++ examples/pinn_inverse/Navier_Stokes_inverse.py
@@ -1,21 +1,19 @@
 """Backend supported: tensorflow.compat.v1, tensorflow, pytorch
 
-Inverse Problem of Navier Stokes Equation Based on Flow Around Cylinder
+An inverse problem of the Navier-Stokes equation of flow around cylinder
 
-training data source：https://doi.org/10.1016/j.jcp.2018.10.045
+References: https://doi.org/10.1016/j.jcp.2018.10.045 Section 4.1.1
 """
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import deepxde as dde
+
 # dde.backend.set_default_backend("tensorflow.compat.v1")
 dde.backend.set_default_backend("tensorflow")
 # dde.backend.set_default_backend("pytorch")
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import loadmat
+import re
 
 # true values
 C1true = 1.0
@@ -24,10 +22,10 @@ C2true = 0.01
 # Load training data
 def load_training_data(num):
     data = loadmat("../dataset/cylinder_nektar_wake.mat")
-    U_star = data['U_star']  # N x 2 x T
-    P_star = data['p_star']  # N x T
-    t_star = data['t']  # T x 1
-    X_star = data['X_star']  # N x 2
+    U_star = data["U_star"]  # N x 2 x T
+    P_star = data["p_star"]  # N x T
+    t_star = data["t"]  # T x 1
+    X_star = data["X_star"]  # N x 2
     N = X_star.shape[0]
     T = t_star.shape[0]
     # Rearrange Data
@@ -42,7 +40,7 @@ def load_training_data(num):
     t = TT.flatten()[:, None]  # NT x 1
     u = UU.flatten()[:, None]  # NT x 1
     v = VV.flatten()[:, None]  # NT x 1
-    p = PP.flatten()[:, None]  # NT x 1 
+    p = PP.flatten()[:, None]  # NT x 1
     # training domain: X × Y = [1, 8] × [−2, 2] and T = [0, 7]
     data1 = np.concatenate([x, y, t, u, v, p], 1)
     data2 = data1[:, :][data1[:, 2] <= 7]
@@ -60,6 +58,7 @@ def load_training_data(num):
     p_train = data_domain[idx, 5:6]
     return [x_train, y_train, t_train, u_train, v_train, p_train]
 
+
 # Parameters to be identified
 C1 = dde.Variable(0.0)
 C2 = dde.Variable(0.0)
@@ -68,7 +67,7 @@ C2 = dde.Variable(0.0)
 def Navier_Stokes_Equation(x, y):
     u = y[:, 0:1]
     v = y[:, 1:2]
-    p = y[:, 2:3]   
+    p = y[:, 2:3]
     du_x = dde.grad.jacobian(y, x, i=0, j=0)
     du_y = dde.grad.jacobian(y, x, i=0, j=1)
     du_t = dde.grad.jacobian(y, x, i=0, j=2)
@@ -76,41 +75,44 @@ def Navier_Stokes_Equation(x, y):
     dv_y = dde.grad.jacobian(y, x, i=1, j=1)
     dv_t = dde.grad.jacobian(y, x, i=1, j=2)
     dp_x = dde.grad.jacobian(y, x, i=2, j=0)
-    dp_y = dde.grad.jacobian(y, x, i=2, j=1)    
+    dp_y = dde.grad.jacobian(y, x, i=2, j=1)
     du_xx = dde.grad.hessian(y, x, component=0, i=0, j=0)
     du_yy = dde.grad.hessian(y, x, component=0, i=1, j=1)
     dv_xx = dde.grad.hessian(y, x, component=1, i=0, j=0)
     dv_yy = dde.grad.hessian(y, x, component=1, i=1, j=1)
-    continuity = du_x + dv_y 
-    x_momentum = du_t + C1 * (u * du_x + v * du_y) +  dp_x - C2 * (du_xx + du_yy)
-    y_momentum = dv_t + C1 * (u * dv_x + v * dv_y) +  dp_y - C2 * (dv_xx + dv_yy)
+    continuity = du_x + dv_y
+    x_momentum = du_t + C1 * (u * du_x + v * du_y) + dp_x - C2 * (du_xx + du_yy)
+    y_momentum = dv_t + C1 * (u * dv_x + v * dv_y) + dp_y - C2 * (dv_xx + dv_yy)
     return [continuity, x_momentum, y_momentum]
 
-# Define Spatio-temporal domain 
+
+# Define Spatio-temporal domain
 # Rectangular
 Lx_min, Lx_max = 1.0, 8.0
 Ly_min, Ly_max = -2.0, 2.0
-# Spatial domain: X × Y = [1, 8] × [−2, 2] 
-space_domain  = dde.geometry.Rectangle([Lx_min, Ly_min], [Lx_max, Ly_max]) 
+# Spatial domain: X × Y = [1, 8] × [−2, 2]
+space_domain = dde.geometry.Rectangle([Lx_min, Ly_min], [Lx_max, Ly_max])
 # Time domain: T = [0, 7]
 time_domain = dde.geometry.TimeDomain(0, 7)
 # Spatio-temporal domain
 geomtime = dde.geometry.GeometryXTime(space_domain, time_domain)
-    
+
 # Get the training data: num = 7000
-[ob_x, ob_y, ob_t, ob_u, ob_v, ob_p] = load_training_data(num = 7000)
+[ob_x, ob_y, ob_t, ob_u, ob_v, ob_p] = load_training_data(num=7000)
 ob_xyt = np.hstack((ob_x, ob_y, ob_t))
 observe_u = dde.icbc.PointSetBC(ob_xyt, ob_u, component=0)
 observe_v = dde.icbc.PointSetBC(ob_xyt, ob_v, component=1)
 
-# Training datasets and Loss 
-data = dde.data.TimePDE( geomtime,
-              Navier_Stokes_Equation, 
-              [observe_u, observe_v],                 
-              num_domain = 700,      
-              num_boundary = 200,     
-              num_initial = 100,      
-              anchors = ob_xyt)         
+# Training datasets and Loss
+data = dde.data.TimePDE(
+    geomtime,
+    Navier_Stokes_Equation,
+    [observe_u, observe_v],
+    num_domain=700,
+    num_boundary=200,
+    num_initial=100,
+    anchors=ob_xyt,
+)
 
 # Neural Network setup
 layer_size = [3] + [50] * 6 + [3]
@@ -125,31 +127,37 @@ variable = dde.callbacks.VariableValue([C1, C2], period=100, filename=fnamevar)
 
 # Compile, train and save model
 model.compile("adam", lr=1e-3, external_trainable_variables=[C1, C2])
-loss_history, train_state = model.train(epochs=10000, callbacks=[variable],
-                                        display_every=1000, disregard_previous_best=True)
+loss_history, train_state = model.train(
+    epochs=10000, callbacks=[variable], display_every=1000, disregard_previous_best=True
+)
 dde.saveplot(loss_history, train_state, issave=True, isplot=True)
 model.compile("adam", lr=1e-4, external_trainable_variables=[C1, C2])
-loss_history, train_state = model.train(epochs=10000, callbacks=[variable],
-                                        display_every=1000, disregard_previous_best=True)
+loss_history, train_state = model.train(
+    epochs=10000, callbacks=[variable], display_every=1000, disregard_previous_best=True
+)
 dde.saveplot(loss_history, train_state, issave=True, isplot=True)
 # model.save(save_path = "./NS_inverse_model/model")
 f = model.predict(ob_xyt, operator=Navier_Stokes_Equation)
 print("Mean residual:", np.mean(np.absolute(f)))
 
 # Plot Variables:
-import re
 # reopen saved data using callbacks in fnamevar
 lines = open(fnamevar, "r").readlines()
-# read output data in fnamevar 
+# read output data in fnamevar
 Chat = np.array(
-      [np.fromstring(min(re.findall(re.escape("[") + "(.*?)" + re.escape("]"), line), key=len), 
-        sep=",",)
-        for line in lines])
+    [
+        np.fromstring(
+            min(re.findall(re.escape("[") + "(.*?)" + re.escape("]"), line), key=len),
+            sep=",",
+        )
+        for line in lines
+    ]
+)
 l, c = Chat.shape
-plt.semilogy(range(0, l*100, 100), Chat[:, 0], "r-")
-plt.semilogy(range(0, l*100, 100), Chat[:, 1], "k-")
-plt.semilogy(range(0, l*100, 100), np.ones(Chat[:, 0].shape) * C1true, "r--")
-plt.semilogy(range(0, l*100, 100), np.ones(Chat[:, 1].shape) * C2true, "k--")
+plt.semilogy(range(0, l * 100, 100), Chat[:, 0], "r-")
+plt.semilogy(range(0, l * 100, 100), Chat[:, 1], "k-")
+plt.semilogy(range(0, l * 100, 100), np.ones(Chat[:, 0].shape) * C1true, "r--")
+plt.semilogy(range(0, l * 100, 100), np.ones(Chat[:, 1].shape) * C2true, "k--")
 plt.legend(["C1hat", "C2hat", "True C1", "True C2"], loc="right")
 plt.xlabel("Epochs")
 plt.title("Variables")
@@ -157,26 +165,26 @@ plt.show()
 
 # Plot trained flow field:
 for t in range(0, 8):
-    [ob_x, ob_y, ob_t, ob_u, ob_v, ob_p] = load_training_data(num = 140000)
+    [ob_x, ob_y, ob_t, ob_u, ob_v, ob_p] = load_training_data(num=140000)
     xyt_pred = np.hstack((ob_x, ob_y, t * np.ones((len(ob_x), 1))))
     uvp_pred = model.predict(xyt_pred)
     x_pred, y_pred, t_pred = xyt_pred[:, 0], xyt_pred[:, 1], xyt_pred[:, 2]
-    u_pred, v_pred, p_pred = uvp_pred[:, 0], uvp_pred[:, 1], uvp_pred[:, 2]      
-    x_true = ob_x[ob_t == t] 
+    u_pred, v_pred, p_pred = uvp_pred[:, 0], uvp_pred[:, 1], uvp_pred[:, 2]
+    x_true = ob_x[ob_t == t]
     y_true = ob_y[ob_t == t]
-    u_true = ob_u[ob_t == t] 
-    fig, ax = plt.subplots(2, 1)  
-    cntr0 = ax[0].tricontourf(x_pred, y_pred, u_pred, levels=80, cmap='rainbow')
+    u_true = ob_u[ob_t == t]
+    fig, ax = plt.subplots(2, 1)
+    cntr0 = ax[0].tricontourf(x_pred, y_pred, u_pred, levels=80, cmap="rainbow")
     cb0 = plt.colorbar(cntr0, ax=ax[0])
-    cntr1 = ax[1].tricontourf(x_true, y_true, u_true, levels=80, cmap='rainbow')
+    cntr1 = ax[1].tricontourf(x_true, y_true, u_true, levels=80, cmap="rainbow")
     cb1 = plt.colorbar(cntr1, ax=ax[1])
-    ax[0].set_title('u-PINN '+'(t='+ str(t) +')', fontsize = 9.5)
-    ax[0].axis('scaled')
-    ax[0].set_xlabel('X', fontsize = 7.5, family = 'Arial')
-    ax[0].set_ylabel('Y', fontsize = 7.5, family = 'Arial')
-    ax[1].set_title('u-Reference solution '+'(t='+ str(t) +')', fontsize = 9.5)
-    ax[1].axis('scaled')
-    ax[1].set_xlabel('X', fontsize = 7.5, family = 'Arial')
-    ax[1].set_ylabel('Y', fontsize = 7.5, family = 'Arial')
+    ax[0].set_title("u-PINN " + "(t=" + str(t) + ")", fontsize=9.5)
+    ax[0].axis("scaled")
+    ax[0].set_xlabel("X", fontsize=7.5, family="Arial")
+    ax[0].set_ylabel("Y", fontsize=7.5, family="Arial")
+    ax[1].set_title("u-Reference solution " + "(t=" + str(t) + ")", fontsize=9.5)
+    ax[1].axis("scaled")
+    ax[1].set_xlabel("X", fontsize=7.5, family="Arial")
+    ax[1].set_ylabel("Y", fontsize=7.5, family="Arial")
     fig.tight_layout()
     plt.show()
