fcaaf600edb49f6a9b175639fc07e68b86aeb2f4
pescap <pescapil@uc.cl>
2022-05-25 08:39:24 -0700

    added warning

--- deepxde/config.py
+++ deepxde/config.py
@@ -47,9 +47,11 @@ def set_default_float(value):
 
 
 def set_random_seed(seed):
-    """Set the global random seeds of random, numpy, and backend.
+    """Set the global random seeds of random, numpy, and backend, so that your code runs deterministically.
 
     - For backend TensorFlow 2.x: Results might change if you run the model several times in the same terminal.
+    Warning:
+    Note that determinism in general comes at the expense of lower performance and so your model may run slower when determinism is enabled.
 
     Args:
         seed (int): The desired seed.
