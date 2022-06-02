c4e5b487323b28c14974cca86c979bb984e542e3
Jerry-Jzy <jzyswufe@163.com>
2022-06-02 19:33:37 -0400

    add deeponet implemented by pytorch

--- deepxde/model.py
+++ deepxde/model.py
@@ -246,8 +246,15 @@ class Model:
 
         def outputs_losses(training, inputs, targets, losses_fn):
             self.net.train(mode=training)
-            inputs = torch.as_tensor(inputs)
-            inputs.requires_grad_()
+
+            if isinstance(inputs,tuple):
+                multi_inputs = torch.as_tensor(inputs[0])
+                inputs_num = len(inputs)
+                for i in range(1,inputs_num):
+                    inputs_i = torch.as_tensor(inputs[i])
+                    inputs_i.requires_grad_()
+                    multi_inputs = (multi_inputs, inputs_i)
+                inputs = multi_inputs
             outputs_ = self.net(inputs)
             # Data losses
             if targets is not None:
--- deepxde/nn/pytorch/__init__.py
+++ deepxde/nn/pytorch/__init__.py
@@ -1,6 +1,7 @@
 """Package for pytorch NN modules."""
 
-__all__ = ["FNN", "NN", "PFNN"]
+__all__ = ["FNN", "NN", "PFNN", "DeepONetCartesianProd", "PODDeepONet"]
 
 from .fnn import FNN, PFNN
 from .nn import NN
+from .deeponet import DeepONetCartesianProd, PODDeepONet
