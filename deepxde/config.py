16983d8f432522dbc84a44ece7950a6df136a933
Lu Lu <lululxvi@gmail.com>
2022-06-18 20:09:22 -0400

    Improve XLA jit

--- deepxde/config.py
+++ deepxde/config.py
@@ -7,14 +7,21 @@ from . import backend as bkd
 from .backend import backend_name, tf, torch, paddle
 from .real import Real
 
+# Default float type
 real = Real(32)
+# Random seed
 random_seed = None
 if backend_name == "jax":
     iinfo = np.iinfo(int)
     jax_random_seed = random.randint(iinfo.min, iinfo.max)
-xla_jit = None
+# XLA
+xla_jit = False
 if backend_name in ["tensorflow.compat.v1", "tensorflow"]:
     xla_jit = bkd.is_gpu_available()
+elif backend_name == "jax":
+    xla_jit = True
+if xla_jit:
+    print("Enable just-in-time compilation with XLA.\n")
 
 
 def default_float():
@@ -47,33 +54,37 @@ def set_default_float(value):
 
 
 def set_random_seed(seed):
-    """Sets all random seeds for the program (Python random, NumPy, and backend), and configures the program to run deterministically.
+    """Sets all random seeds for the program (Python random, NumPy, and backend), and
+    configures the program to run deterministically.
 
-    You can use this to make the program fully deterministic. This means that if the program is run multiple times with the same
-    inputs on the same hardware, it will have the exact same outputs each time. This is useful for debugging models, and for
+    You can use this to make the program fully deterministic. This means that if the
+    program is run multiple times with the same inputs on the same hardware, it will
+    have the exact same outputs each time. This is useful for debugging models, and for
     obtaining fully reproducible results.
 
-    - For backend TensorFlow 2.x: Results might change if you run the model several times in the same terminal.
+    - For backend TensorFlow 2.x: Results might change if you run the model several
+      times in the same terminal.
 
     Warning:
-        Note that determinism in general comes at the expense of lower performance and so your model
-        may run slower when determinism is enabled.
+        Note that determinism in general comes at the expense of lower performance and
+        so your model may run slower when determinism is enabled.
 
     Args:
         seed (int): The desired seed.
     """
-    random.seed(seed)  # python random
-    np.random.seed(seed)  # numpy
+    random.seed(seed)
+    np.random.seed(seed)
     if backend_name == "tensorflow.compat.v1":
         os.environ["TF_DETERMINISTIC_OPS"] = "1"
+        # Based on https://docs.nvidia.com/deeplearning/frameworks/tensorflow-release-notes/rel_19.06.html,
+        # if we set TF_DETERMINISTIC_OPS=1, then there is no need to also set
+        # TF_CUDNN_DETERMINISTIC=1. However, our experiment shows that
+        # TF_CUDNN_DETERMINISTIC=1 is still required.
         os.environ["TF_CUDNN_DETERMINISTIC"] = "1"
-        # Based on https://docs.nvidia.com/deeplearning/frameworks/tensorflow-release-notes/rel_19.06.html, 
-        # if we set TF_DETERMINISTIC_OPS=1 then there is no need to also set TF_CUDNN_DETERMINISM=1. 
-        # However, our experiment shows that TF_CUDNN_DETERMINISM=1 is still required.
-        tf.set_random_seed(seed)  # tf CPU seed
+        tf.set_random_seed(seed)
     elif backend_name == "tensorflow":
         os.environ["TF_DETERMINISTIC_OPS"] = "1"
-        tf.random.set_seed(seed)  # tf CPU seed
+        tf.random.set_seed(seed)
     elif backend_name == "pytorch":
         torch.manual_seed(seed)
     elif backend_name == "jax":
@@ -90,7 +101,12 @@ def enable_xla_jit(mode=True):
 
     - For backend TensorFlow 1.x, by default, compiles with XLA when running on GPU.
       XLA compilation can only be enabled when running on GPU.
-    - For backend TensorFlow 2.x, by default, compiles with XLA when running on GPU.
+    - For backend TensorFlow 2.x, by default, compiles with XLA when running on GPU. If
+      compilation with XLA makes your code slower on GPU, in addition to calling
+      ``disable_xla_jit``, you may simultaneously try XLA with auto-clustering via
+
+          $ TF_XLA_FLAGS=--tf_xla_auto_jit=2 path/to/your/program
+
     - Backend JAX always uses XLA.
     - Backends PyTorch and PaddlePaddle do not support XLA.
 
@@ -98,8 +114,29 @@ def enable_xla_jit(mode=True):
         mode (bool): Whether to enable just-in-time compilation with XLA (``True``) or
             disable just-in-time compilation with XLA (``False``).
     """
+    if backend_name == "tensorflow.compat.v1":
+        if mode and not bkd.is_gpu_available():
+            raise ValueError(
+                "For backend TensorFlow 1.x, XLA compilation can only be enabled when "
+                "running on GPU."
+            )
+    elif backend_name == "tensorflow":
+        if not mode:
+            mode = None
+    elif backend_name == "pytorch":
+        raise ValueError("Backend PyTorch does not support XLA.")
+    elif backend_name == "jax":
+        if not mode:
+            raise ValueError("Backend JAX always uses XLA.")
+    elif backend_name == "paddle":
+        raise ValueError("Backend PaddlePaddle does not support XLA.")
+
     global xla_jit
     xla_jit = mode
+    if xla_jit:
+        print("Enable just-in-time compilation with XLA.\n")
+    else:
+        print("Disable just-in-time compilation with XLA.\n")
 
 
 def disable_xla_jit():
@@ -107,7 +144,12 @@ def disable_xla_jit():
 
     - For backend TensorFlow 1.x, by default, compiles with XLA when running on GPU.
       XLA compilation can only be enabled when running on GPU.
-    - For backend TensorFlow 2.x, by default, compiles with XLA when running on GPU.
+    - For backend TensorFlow 2.x, by default, compiles with XLA when running on GPU. If
+      compilation with XLA makes your code slower on GPU, in addition to calling
+      ``disable_xla_jit``, you may simultaneously try XLA with auto-clustering via
+
+          $ TF_XLA_FLAGS=--tf_xla_auto_jit=2 path/to/your/program
+
     - Backend JAX always uses XLA.
     - Backends PyTorch and PaddlePaddle do not support XLA.
 
