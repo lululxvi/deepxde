7c012788cc1db7b4879d15c261d6b2db991d70e9
Jerry-Jzy <jzyswufe@163.com>
2022-06-28 15:12:38 -0400

    backend Pytorch support MIONetCartesianProd and PODMIONet

--- deepxde/nn/pytorch/__init__.py
+++ deepxde/nn/pytorch/__init__.py
@@ -1,7 +1,8 @@
 """Package for pytorch NN modules."""
 
-__all__ = ["DeepONetCartesianProd", "FNN", "NN", "PFNN", "PODDeepONet"]
+__all__ = ["DeepONetCartesianProd", "FNN", "NN", "PFNN", "PODDeepONet","MIONetCartesianProd"]
 
 from .deeponet import DeepONetCartesianProd, PODDeepONet
+from .mionet import MIONetCartesianProd
 from .fnn import FNN, PFNN
 from .nn import NN
