e1da975be1c7d214aaba7dade3cc74e01b787840
Jerry-Jzy <jzyswufe@163.com>
2022-06-28 15:24:27 -0400

    change nn/pytorch/__init__.py

--- deepxde/nn/pytorch/__init__.py
+++ deepxde/nn/pytorch/__init__.py
@@ -1,8 +1,8 @@
 """Package for pytorch NN modules."""
 
-__all__ = ["DeepONetCartesianProd", "FNN", "NN", "PFNN", "PODDeepONet","MIONetCartesianProd"]
+__all__ = ["DeepONetCartesianProd", "FNN", "NN", "PFNN", "PODDeepONet","PODMIONet","MIONetCartesianProd"]
 
 from .deeponet import DeepONetCartesianProd, PODDeepONet
-from .mionet import MIONetCartesianProd
+from .mionet import MIONetCartesianProd, PODMIONet
 from .fnn import FNN, PFNN
 from .nn import NN
