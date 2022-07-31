5ad1eeb20c101ffc43032d1311ae66290fde29c2
Jerry-Jzy <jzyswufe@163.com>
2022-07-31 18:28:27 -0400

    change port net-->merge net

--- deepxde/nn/pytorch/mionet.py
+++ deepxde/nn/pytorch/mionet.py
@@ -89,14 +89,14 @@ class PODMIONet(NN):
         pod_basis,
         layer_sizes_branch1,
         layer_sizes_branch2,
-        layer_sizes_port,
         activation,
         kernel_initializer,
         connect_method="mul",
         layer_sizes_trunk=None,
         regularization=None,
         trunk_last_activation=False,
-        port_net_after_branch=False
+        layer_sizes_merge=None,
+        merge_net_after_branch=False,
     ):
         super().__init__()
 
@@ -104,7 +104,7 @@ class PODMIONet(NN):
             self.activation_branch1 = activations.get(activation["branch1"])
             self.activation_branch2 = activations.get(activation["branch2"])
             self.activation_trunk = activations.get(activation["trunk"])
-            self.activation_port = activations.get(activation["port"])
+            self.activation_merge = activations.get(activation["merge"])
         else:
             self.activation_branch1 = (
                 self.activation_branch2
@@ -126,15 +126,15 @@ class PODMIONet(NN):
             self.branch2 = FNN(
                 layer_sizes_branch2, self.activation_branch2, kernel_initializer
             )
-        self.port_net_after_branch = port_net_after_branch
-        if self.port_net_after_branch is True:
-            if callable(layer_sizes_port[1]):
+        self.merge_net_after_branch = merge_net_after_branch
+        if self.merge_net_after_branch is True:
+            if callable(layer_sizes_merge[1]):
                 # User-defined network
-                self.port = layer_sizes_port[1]
+                self.merge = layer_sizes_merge[1]
             else:
                 # Fully connected network
-                self.port = FNN(
-                    layer_sizes_port, self.activation_port, kernel_initializer
+                self.merge = FNN(
+                    layer_sizes_merge, self.activation_merge, kernel_initializer
                 )
         self.trunk = None
         if layer_sizes_trunk is not None:
@@ -154,25 +154,27 @@ class PODMIONet(NN):
         y_func1 = self.branch1(x_func1)
         y_func2 = self.branch2(x_func2)
         # connect two branch outputs
-        if self.connect_method =="cat":
-        	x_port = torch.cat((y_func1,y_func2),1)
+        if self.connect_method == "cat":
+            x_merge = torch.cat((y_func1, y_func2), 1)
         else:
             if y_func1.shape[-1] != y_func2.shape[-1]:
                 raise AssertionError(
                     "Output sizes of branch1 net and branch2 net do not match."
                 )
             if self.connect_method == "sum":
-                x_port = y_func1+y_func2
-            elif self.connect_method =="mul":
-                x_port = torch.mul(y_func1,y_func2)
+                x_merge = y_func1 + y_func2
+            elif self.connect_method == "mul":
+                x_merge = torch.mul(y_func1, y_func2)
             else:
-                raise NotImplimentedError(f"{self.connect_method} method to be implimented")
-
-        if self.port_net_after_branch is True:
-            y_func = self.port(x_port)
+                raise NotImplementedError(
+                    f"{self.connect_method} method to be implimented"
+                )
+        # Optional merge net
+        if self.merge_net_after_branch is True:
+            y_func = self.merge(x_merge)
         else:
-            y_func = x_port
-        #Dot product
+            y_func = x_merge
+        # Dot product
         if self.trunk is None:
             # POD only
             y = torch.einsum("bi,ni->bn", y_func, self.pod_basis)
