7f376a2edf70f61a94089c2298e914d8e762da55
Tarik Sahin <tarik.sahin@unibw.de>
2022-11-03 09:59:47 +0100

    Enforce Neumann BC using PointSetOperatorBC

---
+++ examples/pinn_forward/Poisson_PointSetOperator_1d.py
@@ -0,0 +1,52 @@
+"""Backend supported: tensorflow.compat.v1, tensorflow, pytorch, paddle"""
+import deepxde as dde
+import numpy as np
+
+
+def pde(x, y):
+    dy_xx = dde.grad.hessian(y, x)
+    return dy_xx - 2
+
+
+def dy_x(x, y, X):
+    dy_x = dde.grad.jacobian(y, x)
+    return dy_x
+
+
+def boundary_l(x, on_boundary):
+    return on_boundary and np.isclose(x[0], -1)
+
+
+def boundary_r(x, on_boundary):
+    return on_boundary and np.isclose(x[0], 1)
+
+
+def func(x):
+    return (x + 1) ** 2
+
+
+def d_func(x):
+    return 2 * (x + 1)
+
+
+geom = dde.geometry.Interval(-1, 1)
+bc_l = dde.icbc.DirichletBC(geom, func, boundary_l)
+boundary_pts = geom.random_boundary_points(2)
+r_boundary_pts = boundary_pts[np.isclose(boundary_pts, 1)].reshape(-1, 1)
+bc_r = dde.icbc.PointSetOperatorBC(r_boundary_pts, d_func(r_boundary_pts), dy_x)
+
+data = dde.data.PDE(
+    geom, pde, [bc_l, bc_r], num_domain=16, num_boundary=2, solution=func, num_test=100
+)
+
+layer_size = [1] + [50] * 3 + [1]
+activation = "tanh"
+initializer = "Glorot uniform"
+net = dde.nn.FNN(layer_size, activation, initializer)
+
+model = dde.Model(data, net)
+
+model.compile("adam", lr=0.001, metrics=["l2 relative error"])
+losshistory, train_state = model.train(iterations=10000)
+
+dde.saveplot(losshistory, train_state, issave=True, isplot=True)
