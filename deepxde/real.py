1b49e2b4115f71d116895990537d216a45e4984e
Jacob G-W <jacoblevgw@gmail.com>
2023-01-28 22:36:19 -0500

    Add 16 bit float support to deepXDE

--- deepxde/real.py
+++ deepxde/real.py
@@ -7,13 +7,19 @@ class Real:
     def __init__(self, precision):
         self.precision = None
         self.reals = None
-        if precision == 32:
+        if precision == 16:
+            self.set_float16()
+        elif precision == 32:
             self.set_float32()
         elif precision == 64:
             self.set_float64()
 
     def __call__(self, package):
         return self.reals[package]
+
+    def set_float16(self):
+        self.precision = 16
+        self.reals = {np: np.float16, bkd.lib: bkd.float16}
 
     def set_float32(self):
         self.precision = 32
