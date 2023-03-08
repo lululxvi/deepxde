2f0cb1816c0065b08bb320de2aa8b39f20bbb7cc
Saransh Chopra <saransh0701@gmail.com>
2023-03-08 16:18:58 +0530

    Clean up

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,8 @@
 import os
 import sys
 from importlib.metadata import version
+
+
 dde_version = version('deepxde')
 
 sys.path.insert(0, os.path.abspath(".."))
@@ -93,7 +95,7 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ["_static"]
+html_static_path = ["_static"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
