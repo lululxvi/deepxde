1c1baba0baa75b24ad7dd1c5718850dac1405d3c
pescap <pescapil@uc.cl>
2023-04-29 07:52:44 -0700

    remove space

--- deepxde/config.py
+++ deepxde/config.py
@@ -25,7 +25,7 @@ if "OMPI_COMM_WORLD_SIZE" in os.environ:
 
             comm = MPI.COMM_WORLD
             tf.compat.v1.disable_eager_execution()  # Without this line, Horovod broadcasting fails.
-            rank = hvd.rank()  # Only single node acceleration supported so far.
+            rank = hvd.rank() # Only single node acceleration supported so far.
             if rank == 0:
                 print(f"\nParallel training with {world_size} processes.\n")
         else:
