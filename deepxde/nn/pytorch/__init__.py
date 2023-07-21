676ebb6d52c4997079e35cace1cb520dd9e840df
See.Looooo <siumabon123@gmail.com>
2023-07-21 00:35:40 -0400

    Backend pytorch: Small fix (#1396)

--- deepxde/nn/pytorch/__init__.py
+++ deepxde/nn/pytorch/__init__.py
@@ -1,6 +1,7 @@
 """Package for pytorch NN modules."""
 
 __all__ = [
+    "DeepONet",
     "DeepONetCartesianProd",
     "FNN",
     "MIONetCartesianProd",
@@ -10,7 +11,7 @@ __all__ = [
     "PODMIONet",
 ]
 
-from .deeponet import DeepONetCartesianProd, PODDeepONet
+from .deeponet import DeepONet, DeepONetCartesianProd, PODDeepONet
 from .mionet import MIONetCartesianProd, PODMIONet
 from .fnn import FNN, PFNN
 from .nn import NN
