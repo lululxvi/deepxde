44bfd0a5cc77cae380502c14f37f00d2aab03349
Mitchell Daneker <mdaneker@seas.upenn.edu>
2023-10-13 10:11:43 -0400

    Update __init__.py
    Added DeepONet strategies to __init__.py for tensorflow 2.x

--- deepxde/nn/tensorflow/__init__.py
+++ deepxde/nn/tensorflow/__init__.py
@@ -1,7 +1,29 @@
 """Package for tensorflow NN modules."""
 
-__all__ = ["DeepONet", "DeepONetCartesianProd", "FNN", "NN", "PFNN", "PODDeepONet"]
+__all__ = [
+    "DeepONet",
+    "DeepONetCartesianProd",
+    "DeepONetStrategy",
+    "FNN",
+    "IndependentStrategy",
+    "NN",
+    "PFNN",
+    "PODDeepONet" "SingleOutputStrategy",
+    "SplitBothStrategy",
+    "SplitBranchStrategy",
+    "SplitTrunkStrategy",
+]
 
-from .deeponet import DeepONet, DeepONetCartesianProd, PODDeepONet
+from .deeponet import (
+    DeepONet,
+    DeepONetCartesianProd,
+    DeepONetStrategy,
+    IndependentStrategy,
+    PODDeepONet,
+    SingleOutputStrategy,
+    SplitBothStrategy,
+    SplitBranchStrategy,
+    SplitTrunkStrategy,
+)
 from .fnn import FNN, PFNN
 from .nn import NN
