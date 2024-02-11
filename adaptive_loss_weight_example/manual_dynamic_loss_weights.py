6a69f428faa4d1bbe38580ff140cb5088b9cdc4d
LE HAI SON <haison19952013@gmail.com>
2024-02-11 23:38:41 +0900

    example of using ManualDynamicLossWeight

---
+++ adaptive_loss_weight_example/manual_dynamic_loss_weights.py
@@ -0,0 +1,74 @@
+"""Backend supported: tensorflow.compat.v1, tensorflow, pytorch, paddle"""
+import sys
+sys.path.insert(0, '../../deepxde_improvement')
+import deepxde as dde
+import matplotlib.pyplot as plt
+import numpy as np
+from deepxde.callbacks import PrintLossWeight, ManualDynamicLossWeight
+dde.config.disable_xla_jit()
+# from deepxde.backend import set_default_backend
+# set_default_backend("tensorflow")
+
+
+def gen_traindata(num):
+    # generate num equally-spaced points from -1 to 1
+    xvals = np.linspace(-1, 1, num).reshape(num, 1)
+    uvals = np.sin(np.pi * xvals)
+    return xvals, uvals
+
+
+def pde(x, y):
+    u, q = y[:, 0:1], y[:, 1:2]
+    du_xx = dde.grad.hessian(y, x, component=0, i=0, j=0)
+    return -du_xx + q
+
+
+def sol(x):
+    # solution is u(x) = sin(pi*x), q(x) = -pi^2 * sin(pi*x)
+    return np.sin(np.pi * x)
+
+
+geom = dde.geometry.Interval(-1, 1)
+
+bc = dde.icbc.DirichletBC(geom, sol, lambda _, on_boundary: on_boundary, component=0)
+ob_x, ob_u = gen_traindata(100)
+observe_u = dde.icbc.PointSetBC(ob_x, ob_u, component=0)
+
+data = dde.data.PDE(
+    geom,
+    pde,
+    [bc, observe_u],
+    num_domain=200,
+    num_boundary=2,
+    anchors=ob_x,
+    num_test=1000,
+)
+
+net = dde.nn.FNN([1, 40, 40, 40, 2], "tanh", "Glorot uniform")
+PrintLossWeight_cb = PrintLossWeight(period = 1)
+ManualDynamicLossWeight_cb = ManualDynamicLossWeight(epoch2change = 5000, value = 1, Loss_idx = 0)
+model = dde.Model(data, net)
+model.compile("adam", lr=0.0001, loss_weights=[0, 100, 1000])
+losshistory, train_state = model.train(iterations=20000, display_every=1, callbacks = [PrintLossWeight_cb, ManualDynamicLossWeight_cb])
+# dde.saveplot(losshistory, train_state, issave=True, isplot=True)
+
+# view results
+x = geom.uniform_points(500)
+yhat = model.predict(x)
+uhat, qhat = yhat[:, 0:1], yhat[:, 1:2]
+
+utrue = np.sin(np.pi * x)
+print("l2 relative error for u: " + str(dde.metrics.l2_relative_error(utrue, uhat)))
+plt.figure()
+plt.plot(x, utrue, "-", label="u_true")
+plt.plot(x, uhat, "--", label="u_NN")
+plt.legend()
+
+qtrue = -np.pi ** 2 * np.sin(np.pi * x)
+print("l2 relative error for q: " + str(dde.metrics.l2_relative_error(qtrue, qhat)))
+plt.figure()
+plt.plot(x, qtrue, "-", label="q_true")
+plt.plot(x, qhat, "--", label="q_NN")
+plt.legend()
+
+plt.show()
