9d262063ae9cddc5fa823101a6c770c2ebb25528
bonneted <bonneteymard.damien@gmail.com>
2024-03-06 13:23:41 +0100

    Add PFNN class to jax backend

--- deepxde/nn/jax/fnn.py
+++ deepxde/nn/jax/fnn.py
@@ -1,6 +1,7 @@
 from typing import Any, Callable
 
 import jax
+import jax.numpy as jnp
 from flax import linen as nn
 
 from .nn import NN
@@ -52,6 +53,94 @@ class FNN(NN):
                 else self._activation(linear(x))
             )
         x = self.denses[-1](x)
+        if self._output_transform is not None:
+            x = self._output_transform(inputs, x)
+        return x
+
+class PFNN(NN):
+    """Parallel fully-connected network that uses independent sub-networks for each
+    network output.
+
+    Args:
+        layer_sizes: A nested list that defines the architecture of the neural network
+            (how the layers are connected). If `layer_sizes[i]` is an int, it represents
+            one layer shared by all the outputs; if `layer_sizes[i]` is a list, it
+            represents `len(layer_sizes[i])` sub-layers, each of which is exclusively
+            used by one output. Note that `len(layer_sizes[i])` should equal the number
+            of outputs. Every number specifies the number of neurons in that layer.
+    """
+
+    layer_sizes: Any
+    activation: Any
+    kernel_initializer: Any
+
+    params: Any = None
+    _input_transform: Callable = None
+    _output_transform: Callable = None
+
+    def setup(self):
+        if len(self.layer_sizes) <= 1:
+            raise ValueError("must specify input and output sizes")
+        if not isinstance(self.layer_sizes[0], int):
+            raise ValueError("input size must be integer")
+        if not isinstance(self.layer_sizes[-1], int):
+            raise ValueError("output size must be integer")
+
+        n_output = self.layer_sizes[-1]
+
+        self._activation = activations.get(self.activation)
+        kernel_initializer = initializers.get(self.kernel_initializer)
+        initializer = jax.nn.initializers.zeros
+
+        def make_dense(unit):
+            return nn.Dense(
+                unit,
+                kernel_init=kernel_initializer,
+                bias_init=initializer,
+            )
+
+        denses = [
+            make_dense(unit)
+            if isinstance(unit, int)
+            else [
+                make_dense(unit[j])
+                for j in range(n_output)
+            ]
+            for unit in self.layer_sizes[1:-1]
+        ]
+
+        if any(isinstance(unit, (list, tuple)) for unit in self.layer_sizes):
+            denses.append([make_dense(1)] * n_output)
+        else:
+            denses.append(make_dense(n_output))
+
+        self.denses = denses #can't assign directly to self.denses because linen list attributes are converted to tuple, see https://github.com/google/flax/issues/524
+
+    def __call__(self, inputs, training=False):
+        x = inputs
+        if self._input_transform is not None:
+            x = self._input_transform(x)
+
+        for layer in self.denses[:-1]:
+            if isinstance(layer, (list, tuple)):
+                if isinstance(x, list):
+                    x = [self._activation(dense(x_)) for dense, x_ in zip(layer, x)]
+                else:
+                    x = [self._activation(dense(x)) for dense in layer]
+            elif isinstance(x, list):
+                x = [self._activation(layer(x_)) for x_ in x]
+            else:
+                x = self._activation(layer(x))
+
+        # output layers
+        if isinstance(x, list):
+            if x[0].ndim == 1:
+                x = jnp.concatenate([f(x_) for f, x_ in zip(self.denses[-1], x)], axis=0)
+            else:
+                x = jnp.concatenate([f(x_) for f, x_ in zip(self.denses[-1], x)], axis=1)
+        else:
+            x = self.denses[-1](x)
+
         if self._output_transform is not None:
             x = self._output_transform(inputs, x)
         return x
