e49df544c609d09caf113adc35f0366dc6af764b
bonneted <bonneteymard.damien@gmail.com>
2024-04-18 13:19:02 +0200

    Add SPINN module to jax/__init__.py

--- deepxde/nn/jax/__init__.py
+++ deepxde/nn/jax/__init__.py
@@ -1,6 +1,6 @@
 """Package for jax NN modules."""
 
-__all__ = ["FNN", "NN", "PFNN"]
+__all__ = ["FNN", "NN", "PFNN", "SPINN"]
 
-from .fnn import FNN, PFNN
+from .fnn import FNN, PFNN, SPINN
 from .nn import NN
