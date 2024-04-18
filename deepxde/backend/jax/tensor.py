c177d86b2e6c13b9757bf1c0035998b1eef4e4c3
bonneted <bonneteymard.damien@gmail.com>
2024-04-18 13:20:50 +0200

    Add concat and stack functions to jax/tensor.py

--- deepxde/backend/jax/tensor.py
+++ deepxde/backend/jax/tensor.py
@@ -61,6 +61,14 @@ def to_numpy(input_tensor):
     return np.asarray(input_tensor)
 
 
+def concat(values, axis):
+    return jnp.concatenate(values, axis=axis)
+
+
+def stack(values, axis):
+    return jnp.stack(values, axis=axis)
+
+
 def elu(x):
     return jax.nn.elu(x)
 
@@ -83,6 +91,10 @@ def silu(x):
 
 def sin(x):
     return jnp.sin(x)
+
+
+def cos(x):
+    return jnp.cos(x)
 
 
 def square(x):
