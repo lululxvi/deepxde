74e04f07040388b0abbf94a52dd403b79c8cce80
Agniv Sarkar <agni.spare@gmail.com>
2024-06-20 01:01:33 -0700

    Update model.py
    Update to satisfy https://www.tensorflow.org/api_docs/python/tf/keras/Model#save_weights

--- deepxde/model.py
+++ deepxde/model.py
@@ -1031,7 +1031,8 @@ class Model:
         Returns:
             string: Path where model is saved.
         """
-        # TODO: backend tensorflow
+        # TODO: Add assertions OR test the ending
+        #       Right now it seems kind of weird.
         save_path = f"{save_path}-{self.train_state.epoch}"
         if protocol == "pickle":
             save_path += ".pkl"
@@ -1042,7 +1043,7 @@ class Model:
                 save_path += ".ckpt"
                 self.saver.save(self.sess, save_path)
             elif backend_name == "tensorflow":
-                save_path += ".ckpt"
+                save_path += ".weights.h5"
                 self.net.save_weights(save_path)
             elif backend_name == "pytorch":
                 save_path += ".pt"
