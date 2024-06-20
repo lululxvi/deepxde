3c2bc5cad1cb0c14d1471f1e488491cb9e9be5c2
Agniv Sarkar <agni.spare@gmail.com>
2024-06-20 13:14:36 -0700

    Update regularizers.py
    using .lower() instead of adding multiple checks for readability.

--- deepxde/nn/regularizers.py
+++ deepxde/nn/regularizers.py
@@ -5,13 +5,13 @@ def get(identifier):
     # TODO: other backends
     if identifier is None:
         return None
-    name, scales = identifier[0], identifier[1:]
+    name, scales = identifier[0].lower(), identifier[1:]
     return (
         tf.keras.regularizers.L1(l1=scales[0])
-        if name == "l1" or name == "L1"
+        if name == "l1"
         else tf.keras.regularizers.L2(l2=scales[0])
-        if name == "l2" or name == "L2"
+        if name == "l2"
         else tf.keras.regularizers.L1L2(l1=scales[0], l2=scales[1])
-        if name == "l1+l2" or name == "L1L2"
+        if name == "l1+l2" or name == "l1l2"
         else None
     )
