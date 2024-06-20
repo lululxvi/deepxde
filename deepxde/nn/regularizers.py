1e5bb89da6050fdba925a1eb32ba854572f69b09
Agniv Sarkar <agni.spare@gmail.com>
2024-06-20 00:08:04 -0700

    Update regularizers.py
    Check the [docs](https://www.tensorflow.org/api_docs/python/tf/keras/regularizers/L1) and the argument is of the form l1/l2 instead of just l in the current version.

--- deepxde/nn/regularizers.py
+++ deepxde/nn/regularizers.py
@@ -7,9 +7,9 @@ def get(identifier):
         return None
     name, scales = identifier[0], identifier[1:]
     return (
-        tf.keras.regularizers.l1(l=scales[0])
+        tf.keras.regularizers.l1(l1=scales[0])
         if name == "l1"
-        else tf.keras.regularizers.l2(l=scales[0])
+        else tf.keras.regularizers.l2(l2=scales[0])
         if name == "l2"
         else tf.keras.regularizers.l1_l2(l1=scales[0], l2=scales[1])
         if name == "l1+l2"
