c773a2f45f2bb93582a3bb1ee30d7c1f309d4a91
lijialin03 <lijialin03@baidu.com>
2024-12-09 02:35:43 +0000

    update docstring

--- deepxde/nn/regularizers.py
+++ deepxde/nn/regularizers.py
@@ -2,7 +2,7 @@ from .. import backend as bkd
 
 
 def get(identifier):
-    """Retrieves a TensorFlow regularizer instance based on the given identifier.
+    """Retrieves a regularizer instance based on the given identifier.
 
     Args:
         identifier (list/tuple): Specifies the type and factor(s) of the regularizer.
@@ -27,7 +27,7 @@ def get(identifier):
     if name == "l2":
         return bkd.l2_regularization(factor[0])
     if name in ("l1l2", "l1+l2"):
-        # TODO: only supported by 'tensorflow.compat.v1' now.
+        # TODO: only supported by 'tensorflow.compat.v1' and 'tensorflow' now.
         if len(factor) < 2:
             raise ValueError("L1L2 regularizer requires both L1/L2 penalties.")
         return bkd.l1_l2_regularization(factor[0], factor[1])
