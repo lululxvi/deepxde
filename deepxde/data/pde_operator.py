68b4fe9816ab3fce198fd9118c585c5347568c5f
Jerry-Jzy <jzyswufe@163.com>
2024-12-15 19:30:45 -0500

    add empty line and change variable names

--- deepxde/data/pde_operator.py
+++ deepxde/data/pde_operator.py
@@ -272,25 +272,25 @@ class PDEOperatorCartesianProd(Data):
             losses = [loss_fn(bkd.zeros_like(error), error) for error in error_f]  # noqa
 
         # BC loss
-        for k, bc in enumerate(self.pde.bcs):
-            beg, end = bcs_start[k], bcs_start[k + 1]
-            error_k = []
+        for j, bc in enumerate(self.pde.bcs):
+            beg, end = bcs_start[j], bcs_start[j + 1]
+            error = []
             for i in range(num_func):
-                output_i = outputs[i]
-                if bkd.ndim(output_i) == 1:  # noqa
-                    output_i = output_i[:, None]
-                error_ki = bc.error(
+                out = outputs[i]
+                if bkd.ndim(out) == 1:  # noqa
+                    out = out[:, None]
+                error_i = bc.error(
                     self.train_x[1],
                     inputs[1],
-                    output_i,
+                    out,
                     beg,
                     end,
                     aux_var=model.net.auxiliary_vars[i][:, None],
                 )
-                error_k.append(error_ki)
-            error_k = bkd.stack(error_k, axis=0)  # noqa
-            loss_k = loss_fn(bkd.zeros_like(error_k), error_k)  # noqa
-            losses.append(loss_k)
+                error.append(error_i)
+            error = bkd.stack(error, axis=0)  # noqa
+            loss = loss_fn(bkd.zeros_like(error), error)  # noqa
+            losses.append(loss)
         return losses
 
     def losses_train(self, targets, outputs, loss_fn, inputs, model, aux=None):
