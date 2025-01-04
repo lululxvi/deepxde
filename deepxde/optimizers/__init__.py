28dee2265c38f783c5d9870aecc87a2f7fd0ddb6
lululxvi <lululxvi@gmail.com>
2025-01-03 21:33:10 -0500

    get Sphinx documentation to build all optimizers

--- deepxde/optimizers/__init__.py
+++ deepxde/optimizers/__init__.py
@@ -5,6 +5,17 @@ from .config import LBFGS_options, set_LBFGS_options, NNCG_options, set_NNCG_opt
 from ..backend import backend_name
 
 
+# To get Sphinx documentation to build, we import all
+if os.environ.get("READTHEDOCS") == "True":
+    # The backend should be tensorflow/tensorflow.compat.v1 to ensure backend.tf is not
+    # None.
+    from . import jax
+    from . import paddle
+    from . import pytorch
+    from . import tensorflow
+    from . import tensorflow_compat_v1
+
+
 def _load_backend(mod_name):
     mod = importlib.import_module(".%s" % mod_name, __name__)
     thismod = sys.modules[__name__]
