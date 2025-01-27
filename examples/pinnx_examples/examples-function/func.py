ce55b50e50ddfde00c8c3379578745504dc00232
routhleck <sichaohe@stu.pku.edu.cn>
2025-01-27 09:41:41 +0800

    Remove license info in source code

--- examples/pinnx_examples/examples-function/func.py
+++ examples/pinnx_examples/examples-function/func.py
@@ -1,18 +1,3 @@
-# Copyright DeepXDE Limited. All Rights Reserved.
-#
-# Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 2.1 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
 import brainstate as bst
 import brainunit as u
 
