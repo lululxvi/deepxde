82e52036545474f005d3f3519176ba9672bf1965
Vladimir <vldud.off@gmail.com>
2025-05-20 19:30:45 +0300

    Backend Pytorch: L-LAAF implementation

--- deepxde/utils/pytorch.py
+++ deepxde/utils/pytorch.py
@@ -1,1 +1,14 @@
 """Utilities of pytorch."""
+
+from ..backend import torch
+
+
+class LLAAF(torch.nn.Module):
+    def __init__(self, activation, n):
+        super().__init__()
+        self.activation = activation
+        self.n = n
+        self.a = torch.nn.Parameter(torch.tensor(1.0 / n))
+
+    def forward(self, x):
+        return self.activation(self.n * self.a * x)
