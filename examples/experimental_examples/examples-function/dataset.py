bd4bfac419b5627722582ea467931bffcc8ab206
routhleck <sichaohe@stu.pku.edu.cn>
2025-06-26 11:34:42 +0800

    splitting #1932 into an independent PR(examples)
    Co-Authored-By: Chaoming Wang <chao.brain@qq.com>

---
+++ examples/experimental_examples/examples-function/dataset.py
@@ -0,0 +1,31 @@
+import os
+
+import brainstate as bst
+import numpy as np
+
+import deepxde.experimental as deepxde
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+train_data = np.loadtxt(os.path.join(PATH, "../..", "dataset", "dataset.train"))
+test_data = np.loadtxt(os.path.join(PATH, "../..", "dataset", "dataset.test"))
+
+net = deepxde.nn.Model(
+    deepxde.nn.DictToArray(x=None),
+    deepxde.nn.FNN([1] + [50] * 3 + [1], "tanh", bst.init.KaimingUniform()),
+    deepxde.nn.ArrayToDict(y=None),
+)
+
+data = deepxde.problem.DataSet(
+    X_train={"x": train_data[:, 0]},
+    y_train={"y": train_data[:, 1]},
+    X_test={"x": test_data[:, 0]},
+    y_test={"y": test_data[:, 1]},
+    standardize=True,
+    approximator=net,
+)
+
+model = deepxde.Trainer(data)
+model.compile(bst.optim.Adam(0.001), metrics=["l2 relative error"]).train(
+    iterations=50000
+)
+model.saveplot(issave=True, isplot=True)
---
+++ examples/experimental_examples/examples-operator/poisson_1d_pideeponet.py
@@ -0,0 +1,96 @@
+import brainstate as bst
+import matplotlib.pyplot as plt
+import numpy as np
+import jax
+import deepxde
+import deepxde.experimental as deepxde_new
+import brainunit as u
+
+
+# Poisson equation: -u_xx = f
+def equation(x, y, aux):
+
+    def solve_hes(inp1):
+        f1 = lambda i: deepxde_new.grad.hessian(
+            lambda inp: net((x[0], inp))["u"][i], inp1, vmap=False
+        )
+        return jax.vmap(f1)(np.arange(x[0].shape[0]))
+
+    dy_xx = jax.vmap(solve_hes, out_axes=1)(jax.numpy.expand_dims(x[1], 1))
+    dy_xx = u.math.squeeze(dy_xx)
+    return -dy_xx - aux
+
+
+# Domain is interval [0, 1]
+geom = deepxde_new.geometry.Interval(0, 1).to_dict_point("x")
+
+bc = deepxde_new.icbc.DirichletBC(lambda x, aux: {"u": 0.0})
+
+# Function space for f(x) are polynomials
+degree = 3
+space = deepxde.data.PowerSeries(N=degree + 1)
+
+# Choose evaluation points
+num_eval_points = 10
+evaluation_points = geom.uniform_points(num_eval_points, boundary=True)
+evaluation_points = deepxde_new.utils.dict_to_array(evaluation_points)
+
+# Setup DeepONet
+dim_x = 1
+p = 32
+net = bst.nn.Sequential(
+    deepxde_new.nn.DeepONetCartesianProd(
+        [num_eval_points, 32, p],
+        [dim_x, 32, p],
+        activation="tanh",
+    ),
+    deepxde_new.nn.ArrayToDict(u=None),
+)
+
+# Define PDE operator
+pde_op = deepxde_new.problem.PDEOperatorCartesianProd(
+    geom,
+    equation,
+    bc,
+    space,
+    evaluation_points,
+    approximator=net,
+    num_function=100,
+    num_domain=100,
+    num_boundary=2,
+)
+
+# Define and train trainer
+model = deepxde_new.Trainer(pde_op)
+model.compile(bst.optim.Adam(0.0005)).train(iterations=20000)
+model.saveplot(isplot=True)
+
+# Plot realisations of f(x)
+n = 3
+features = space.random(n)
+fx = space.eval_batch(features, evaluation_points)
+
+x = geom.uniform_points(100, boundary=True)
+y = model.predict((fx, x))["u"]
+
+# Setup figure
+fig = plt.figure(figsize=(7, 8))
+plt.subplot(2, 1, 1)
+plt.title("Poisson equation: Source term f(x) and solution u(x)")
+plt.ylabel("f(x)")
+z = np.zeros_like(x)
+plt.plot(x, z, "k-", alpha=0.1)
+
+# Plot source term f(x)
+for i in range(n):
+    plt.plot(evaluation_points, fx[i], "--")
+
+# Plot solution u(x)
+plt.subplot(2, 1, 2)
+plt.ylabel("u(x)")
+plt.plot(x, z, "k-", alpha=0.1)
+for i in range(n):
+    plt.plot(x, y[i], "-")
+plt.xlabel("x")
+
+plt.show()
