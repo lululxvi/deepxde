f0b35f26f2757747d55d12ff1fad785afcff3bfe
wengyao <wengyaojiang@gmail.com>
2025-11-30 22:32:21 -0500

    fix the build

--- docs/conf.py
+++ docs/conf.py
@@ -102,7 +102,6 @@ html_static_path = ["_static"]
 
 # Exclude imports
 # autodoc_mock_imports = ["tensorflow"]
-autodoc_mock_imports = ["paddle"]
 
 # -- Options for HTMLHelp output ---------------------------------------------
 
