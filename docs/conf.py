b0ca22167488f9d8be035f1f18f9d4cdeebb240e
wengyao <wengyaojiang@gmail.com>
2025-12-04 19:19:04 -0500

    test the need of os.environ[CUDA_VISIBLE_DEVICES]

--- docs/conf.py
+++ docs/conf.py
@@ -12,11 +12,6 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-# disable CUDA/GPU for Read the Docs build
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-# os.environ["DDE_BACKEND"] = "tensorflow"
-
 from importlib.metadata import version
 
 
